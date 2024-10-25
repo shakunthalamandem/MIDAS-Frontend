00a833e5d7dc8e1c194d8e80a07f10e2adfeb813
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2024-10-25 15:24:12 +0530

    remoed the jest

--- Frontend/my-app/src/Components/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|scss|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
-    },
-  };
-  
