d5db7ba32965e41375835a66b66281bfdebf2401
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2024-12-04 16:17:19 +0530

    added the colors

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/MDDFilters.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/MDDFilters.tsx
@@ -111,7 +111,7 @@ const MDDFilters: React.FC<FiltersProps> = ({ filtersData, apiName }) => {
   return (
     <Container maxWidth="xl" sx={{ padding: 0, marginBottom: 20, display: "flex", marginLeft: 0, marginTop: 10 }}>
       <Box width="400px" sx={{ marginRight: 10 }}>
-        <Card sx={{ borderRadius: 2, boxShadow: 3 }}>
+        <Card sx={{ borderRadius: 2, boxShadow: 3 ,backgroundColor:'#e6ebf5'}}>
           <CardContent>
             <Box width="300px" sx={{ p: 2 }}>
               <Typography variant="h5" color="#002060">Filters</Typography>
