3f77a2d6489c9da51c173f945c5054650fcb1065
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2024-12-20 12:11:56 +0530

    added the basic setup

---
+++ Frontend/my-app/src/Components/Main/InvestmentStrategy/InvestmentMain.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvestmentMain = () => {
+  return (
+    <div>InvestmentMain</div>
+  )
+}
+
+export default InvestmentMain
---
+++ Frontend/my-app/src/Components/Main/InvestmentStrategy/InvestmentTabs.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvestmentTabs = () => {
+  return (
+    <div>InvestmentTabs</div>
+  )
+}
+
+export default InvestmentTabs
