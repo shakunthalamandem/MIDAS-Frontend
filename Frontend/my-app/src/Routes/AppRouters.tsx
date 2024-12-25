b93da9db31d6d35c30ffdbcdfc51ce99865fda9d
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2024-12-25 14:57:24 +0530

    added the rouutes

--- Frontend/my-app/src/Routes/AppRouters.tsx
+++ Frontend/my-app/src/Routes/AppRouters.tsx
@@ -8,6 +8,7 @@ import Strategies from '../Components/Main/HomePage/Dashboard/Strategies';
 import Login from '../Components/Main/HomePage/Authentication/Login';
 import CapitalMarketsStatic from '../Components/HomepageStatic/CapitalMarketsStatic';
 import InvestmentMain from '../Components/Main/InvestmentStrategy/InvestmentMain';
+import TechnicalMain from '../Components/Main/InvestmentStrategy/TechnicalIndicators/TechnicalMain';
 
 
 const AppRouters: React.FC = () => {
@@ -19,6 +20,8 @@ const AppRouters: React.FC = () => {
         <Route path="/capital-markets" element={<CapitalMarkets />} />
         <Route path="/monashee-deals" element={<MonasheeDeals />} />
         <Route path="/strategies" element={<InvestmentMain />} />
+        <Route path="/technicalanalysis" element={<TechnicalMain />} />
+
 
       </Routes>
   );
