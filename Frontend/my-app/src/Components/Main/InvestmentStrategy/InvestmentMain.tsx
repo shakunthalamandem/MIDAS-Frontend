f1505d524d7617b494ed386d9ae8e2c7ce112e1c
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2024-12-25 14:43:27 +0530

    fixed the           Investment Strategies title

--- Frontend/my-app/src/Components/Main/InvestmentStrategy/InvestmentMain.tsx
+++ Frontend/my-app/src/Components/Main/InvestmentStrategy/InvestmentMain.tsx
@@ -18,12 +18,20 @@ const InvestmentMain: React.FC = () => {
   return (
     <>
       {/* Header */}
-      <Box
+   
+          <Typography
+        variant="h3"
         sx={{
+          fontWeight: "bold",
+          color: "#FFFFFF",
+          fontSize: { xs: "2rem" },
           backgroundColor: "#002060",
-          padding: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "5vh",
           textAlign: "center",
-          marginBottom: 2,
+          marginBottom: "10px",
           animation: "fadeInScale 2s ease-out",
           "@keyframes fadeInScale": {
             "0%": { opacity: 0, transform: "scale(0.8)" },
@@ -31,17 +39,8 @@ const InvestmentMain: React.FC = () => {
           },
         }}
       >
-        <Typography
-          variant="h3"
-          sx={{
-            fontWeight: "bold",
-            color: "#FFFFFF",
-            fontSize: { xs: "2rem", sm: "2.5rem" },
-          }}
-        >
           Investment Strategies
-        </Typography>
-      </Box>
+          </Typography>
 
       {/* Fetch Data */}
       <ScreenerJsonData onDataLoaded={handleDataLoaded} />
