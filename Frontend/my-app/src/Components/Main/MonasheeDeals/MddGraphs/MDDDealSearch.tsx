4baa69ac35f2a01754cdd4f2de6bfe5703624925
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-01-06 17:11:33 +0530

    added

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MddGraphs/MDDDealSearch.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MddGraphs/MDDDealSearch.tsx
@@ -64,43 +64,46 @@ const MDDDealSearch: React.FC = () => {
           variant="outlined"
           value={searchTerm}
           onChange={handleSearch}
-          fullWidth
-          style={{ marginBottom: "20px" }}
+          style={{ marginBottom: "20px",
+            minWidth:'300px' 
+          }}
         />
         {loading ? (
-          <CircularProgress />
-        ) : (
-          <Paper
-            elevation={3}
+  <CircularProgress />
+) : (
+  searchTerm.length > 0 && ( // Show Paper for any non-empty search term
+    <Paper
+      elevation={3}
+      style={{
+        padding: "10px",
+        maxWidth: "300px",
+        maxHeight: "300px", // Limit the height of the options
+        overflowY: "auto", // Add scroll if the content overflows
+      }}
+    >
+      <List>
+        {results.map((item, index) => (
+          <ListItem
+            key={index}
+            onClick={() => handleItemClick(item.ticker_us)} // Pass only ticker_us
+            component="li"
             style={{
-              padding: "10px",
-              maxHeight: "300px", // Limit the height of the options
-              overflowY: "auto", // Add scroll if the content overflows
+              backgroundColor:
+                selectedTicker === item.ticker_us
+                  ? "rgba(63, 81, 181, 0.1)" // Light blue background for selected item
+                  : "transparent",
             }}
           >
-            <List>
-              {results.map((item, index) => (
-                <ListItem
-                  key={index}
-                  onClick={() => handleItemClick(item.ticker_us)} // Pass only ticker_us
-                  component="li"
-                  
-                  style={{
-                    backgroundColor:
-                      selectedTicker === item.ticker_us
-                        ? "rgba(63, 81, 181, 0.1)" // Light blue background for selected item
-                        : "transparent",
-                  }}
-                >
-                  <ListItemText
-                    primary={<strong>{item.ticker_us}</strong>}
-                    secondary={item.issuer_name}
-                  />
-                </ListItem>
-              ))}
-            </List>
-          </Paper>
-        )}
+            <ListItemText
+              primary={<strong>{item.ticker_us}</strong>}
+              secondary={item.issuer_name}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Paper>
+  )
+)}
       </Box>
 
       {/* If a ticker is selected, render the SelectedTicker component */}
