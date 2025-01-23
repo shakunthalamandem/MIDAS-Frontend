717c562289442eededb91dc403dbf42b95ecefa4
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-01-23 18:12:52 +0530

    rmeoved the comments

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MddGraphs/MDDScrenner/DealSpecificTab.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MddGraphs/MDDScrenner/DealSpecificTab.tsx
@@ -145,16 +145,16 @@ const DealSpecificTab: React.FC<DealSpecificTabProps> = ({ filtersData }) => {
                       variant="outlined"
                       size="small"
                       value={field.value || ""}
-                      // onChange={(e) => {
-                      //   const value = e.target.value ? parseFloat(e.target.value) : null;
-                      //   const currentValues = form.values[key] || [null, null];
-                      //   const updatedValues = [...currentValues];
-                      //   updatedValues[index] = value;
-                      //   form.setFieldValue(
-                      //     key,
-                      //     updatedValues.map((v, i) => (v === "" ? null : v))
-                      //   );
-                      // }}
+                      onChange={(e) => {
+                        const value = e.target.value ? parseFloat(e.target.value) : null;
+                        const currentValues = form.values[key] || [null, null];
+                        const updatedValues = [...currentValues];
+                        updatedValues[index] = value;
+                        form.setFieldValue(
+                          key,
+                          updatedValues.map((v, i) => (v === "" ? null : v))
+                        );
+                      }}
                       sx={{
                         maxWidth: "100px",
                         "& input": {
