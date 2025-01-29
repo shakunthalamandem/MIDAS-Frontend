0d47f77c5e75ef6711969c39a667c3d656b05958
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-01-29 09:54:05 +0530

    css

--- Frontend/my-app/src/Components/Main/HomePage/Dashboard/MonasheeDeals.tsx
+++ Frontend/my-app/src/Components/Main/HomePage/Dashboard/MonasheeDeals.tsx
@@ -131,7 +131,7 @@ const MonasheeDeals: React.FC = () => {
       >
         <Tab sx={{ backgroundColor: value === 0 ? "#dce6f0" : "#f5f5f5", color: value === 0 ? "#fff" : "#777", "&.Mui-selected": { backgroundColor: "#dce6f0", color: "#fff" } }} 
           label={
-            <Box sx={{ width: "100%", padding: 2, maxHeight: "45px", minWidth: "230px", }}>
+            <Box sx={{ width: "100%", maxHeight: "45px", maxWidth: "200px"}}>
               <TextField
                 label=""
                 variant="outlined"
@@ -141,7 +141,7 @@ const MonasheeDeals: React.FC = () => {
                 placeholder="Enter ticket symbol..."
                 style={{
                   marginBottom: "1px",
-                  width: "250px", // Increase
+                  width: "200px", // Increase
                   height: "40px", // Slightly
                   borderRadius: "32px", // In
                   backgroundColor: "#f4f6f9", // This
@@ -151,7 +151,7 @@ const MonasheeDeals: React.FC = () => {
                 InputProps={{
                   style: {
                     borderRadius: "42px",
-                    width: "250px", // Increase
+                    width: "200px",
                     height: "40px",
                   },
                   startAdornment: (
