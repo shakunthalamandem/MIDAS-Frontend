f00a1d4f9a2365b6ae5cb5a978b1b15a38d94993
tharunsai08 <tharunsai.k@goldenhillsindia.com>
2025-02-03 12:28:29 +0530

    added borders to allocation cap and changed the deal voume to deal size

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealStatsGraph.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealStatsGraph.tsx
@@ -61,7 +61,7 @@ const DealStatsGraph: React.FC<DealAllocationGraphProps> = ({
   const dealStatsOptions = [
     { label: "Deals Count", key: "count" },
     { label: "Deal Volume", key: "deal_size" },
-    { label: "Average Deal Volume", key: "avg_deal_size" },
+    { label: "Average Deal Size", key: "avg_deal_size" },
     { label: "Allocation as % of Deal Size", key: "mdd_allocation_percentage" },
     { label: "Allocation as % of IOI", key: "mdd_allocation_ioi" },
   ];
