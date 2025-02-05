eea418d1cd97199629e74cca9ec6a5ed6fa3bfb9
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-05 15:54:03 +0530

    added

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/MDDScreenergrid.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/MDDScreenergrid.tsx
@@ -129,9 +129,9 @@ const MDDScreenergrid: React.FC<MDDScreenergridProps> = ({
   };
 
   const columns: GridColDef[] = [
-    { field: "pricing_date", headerName: "Pricing Date", width: 150 },
-    { field: "issuer_name", headerName: "Issuer Name", width: 200 },
     { field: "ticker", headerName: "Ticker", width: 150 },
+    { field: "issuer_name", headerName: "Issuer Name", width: 200 },
+    { field: "pricing_date", headerName: "Pricing Date", width: 150 },
     {
       field: "gics_sector_from_bloomberg",
       headerName: "Sector",
