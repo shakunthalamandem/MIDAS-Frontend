c81dc1fb8896ac7586b146ae4f6dc4d3470572d9
charangoldenhills <charan.d@goldenhillsindia.com>
2025-02-06 12:06:37 +0530

    weekly chart added but need changes.

--- Frontend/my-app/src/Components/Main/HomePage/Dashboard/MonasheeDeals.tsx
+++ Frontend/my-app/src/Components/Main/HomePage/Dashboard/MonasheeDeals.tsx
@@ -6,6 +6,7 @@ import AllocationCaptureReturn from "../../MonasheeDeals/MddGraphs/AllocationCap
 import FOllowOnDiscount from "../../MonasheeDeals/MddGraphs/FOllowOnDiscount";
 import DealStats from "../../MonasheeDeals/MddGraphs/DealStats";
 import MDDSelectedTicker from "../../MonasheeDeals/MddGraphs/MDDSelectedTicker";
+import WeeklyStatsChart from "../../MonasheeDeals/MDDSettings/WeeklyStatsChart";
 
 // Define the type for the API response
 interface MDDResult {
@@ -173,6 +174,7 @@ const token = localStorage.getItem("access_token");
         <Tab label="Deal Stats" />
         <Tab label="GAP Analysis" />
         <Tab label="Follow-On Discount" />
+        <Tab label="Weekly Deals" />
         <Tab label="Screener" />
       </Tabs>
       {searchTerm.length > 0 && (
@@ -239,7 +241,8 @@ const token = localStorage.getItem("access_token");
       {value === 1 && <DealStats />}
       {value === 2 && <AllocationCaptureReturn />}
       {value === 3 && <FOllowOnDiscount />}
-      {value === 4 && <MDDScreener />}
+      {value === 4 && <WeeklyStatsChart />}
+      {value === 5 && <MDDScreener />}
     </Box>
   );
 };
