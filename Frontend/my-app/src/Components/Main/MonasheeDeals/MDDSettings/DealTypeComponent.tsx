73d2ba4388105b35fcd73844576d2bf2dd5b9b58
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-10 11:13:45 +0530

    added css

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealTypeComponent.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealTypeComponent.tsx
@@ -54,7 +54,7 @@ const DealTypeComponent: React.FC<{ data: Data }> = ({ data = {} }) => {
 
         return (
           <Paper key={year} sx={{ padding: 5, marginBottom: 3, width: "90%", background: "#F5E8DC" }}> 
-            <Typography variant="h6" align="center" sx={{ color: "#002060" }}>
+            <Typography variant="h6" align="center" sx={{ color: "#002060",fontWeight:'bold' }}>
               {`GAP Analysis for ${year}`}
             </Typography>
             <RadioGroup
