4e3ed2029944c43045c4cd8ad6a8af983c763591
ghcit2025 <ghcit@goldenhillsindia.com>
2025-02-10 16:11:38 +0530

    wrong spell

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealStatsGraph.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealStatsGraph.tsx
@@ -96,7 +96,7 @@ const filterOptions: FilterOption[] = [
     payload: { filter_type: "broad_region" },
   },
   {
-    label: "Deal Captian",
+    label: "Deal Captain",
     value: "caption",
     payload: { filter_type: "deal_captain" },
   },
