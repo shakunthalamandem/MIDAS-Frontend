1886f83aa6f2daa9f5f98f087482485d1e821583
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-10 15:15:39 +0530

    added the ipo and fo buttons

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealTypeComponent.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DealTypeComponent.tsx
@@ -7,19 +7,19 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Radio,
-  RadioGroup,
-  FormControlLabel,
   Typography,
   Box,
+  Button,
 } from "@mui/material";
 
 const formatValue = (value: number): string => {
   const absValue = Math.abs(value);
   const sign = value < 0 ? "-" : "";
 
-  if (absValue >= 1_000_000_000) return `${sign}$${(absValue / 1_000_000_000).toFixed(1)}B`;
-  if (absValue >= 1_000_000) return `${sign}$${(absValue / 1_000_000).toFixed(1)}M`;
+  if (absValue >= 1_000_000_000)
+    return `${sign}$${(absValue / 1_000_000_000).toFixed(1)}B`;
+  if (absValue >= 1_000_000)
+    return `${sign}$${(absValue / 1_000_000).toFixed(1)}M`;
   if (absValue >= 1_000) return `${sign}$${(absValue / 1_000).toFixed(1)}K`;
 
   return `${sign}$${absValue.toFixed(2)}`;
@@ -36,10 +36,12 @@ interface Data {
 }
 
 const DealTypeComponent: React.FC<{ data: Data }> = ({ data = {} }) => {
-  const sortedYears = Object.keys(data).sort((a, b) => parseInt(b) - parseInt(a));
-  const [selectedTypes, setSelectedTypes] = useState<{ [year: string]: "IPO" | "FO" }>(
-    sortedYears.reduce((acc, year) => ({ ...acc, [year]: "IPO" }), {})
+  const sortedYears = Object.keys(data).sort(
+    (a, b) => parseInt(b) - parseInt(a)
   );
+  const [selectedTypes, setSelectedTypes] = useState<{
+    [year: string]: "IPO" | "FO";
+  }>(sortedYears.reduce((acc, year) => ({ ...acc, [year]: "IPO" }), {}));
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
@@ -53,39 +55,117 @@ const DealTypeComponent: React.FC<{ data: Data }> = ({ data = {} }) => {
         });
 
         return (
-          <Paper key={year} sx={{ padding: 5, marginBottom: 3, width: "90%", background: "#F5E8DC" }}> 
-            <Typography variant="h6" align="center" sx={{ color: "#002060",fontWeight:'bold' }}>
+          <Paper
+            key={year}
+            sx={{
+              padding: 5,
+              marginBottom: 3,
+              width: "90%",
+              background: "#F5E8DC",
+            }}
+          >
+            <Typography
+              variant="h6"
+              align="center"
+              sx={{ color: "#002060", fontWeight: "bold" }}
+            >
               {`GAP Analysis for ${year}`}
             </Typography>
-            <RadioGroup
-              row
-              value={selectedType}
-              onChange={(e) =>
-                setSelectedTypes((prev) => ({ ...prev, [year]: e.target.value as "IPO" | "FO" }))
-              }
-              sx={{ justifyContent: "center", marginBottom: 2 }}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                gap: 1,
+                marginBottom: 2,
+                marginTop:3
+              }}
             >
-              <FormControlLabel value="IPO" control={<Radio />} label="IPO" />
-              <FormControlLabel value="FO" control={<Radio />} label="FO" />
-            </RadioGroup>
+              <Button
+                variant={selectedType === "IPO" ? "contained" : "outlined"}
+                sx={{
+                  backgroundColor:
+                    selectedType === "IPO" ? "#a204fc" : "transparent",
+                  color: selectedType === "IPO" ? "#fff" : "#a204fc",
+                  borderColor: "#a204fc",
+                  "&:hover": {
+                    backgroundColor: "#a204fc",
+                    color: "#fff",
+                  },
+                }}
+                onClick={() =>
+                  setSelectedTypes((prev) => ({ ...prev, [year]: "IPO" }))
+                }
+              >
+                IPO
+              </Button>
+
+              <Button
+                variant={selectedType === "FO" ? "contained" : "outlined"}
+                sx={{
+                  backgroundColor:
+                    selectedType === "FO" ? "#a204fc" : "transparent",
+                  color: selectedType === "FO" ? "#fff" : "#a204fc",
+                  borderColor: "#a204fc",
+                  "&:hover": {
+                    backgroundColor: "#a204fc",
+                    color: "#fff",
+                  },
+                }}
+                onClick={() =>
+                  setSelectedTypes((prev) => ({ ...prev, [year]: "FO" }))
+                }
+              >
+                FO
+              </Button>
+            </Box>
 
             <TableContainer component={Paper} sx={{ border: "1px solid #ccc" }}>
               <Table size="small">
                 <TableHead>
                   <TableRow sx={{ backgroundColor: "#1976d2" }}>
                     <TableCell sx={{ color: "white" }}>Quintile</TableCell>
-                    <TableCell sx={{ color: "white" }}>T+1M Absolute Returns</TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      T+1M Absolute Returns
+                    </TableCell>
                     <TableCell sx={{ color: "white" }}>No of Deals</TableCell>
-                    <TableCell sx={{ color: "white" }}>Deal Volume ($)</TableCell>
-                    <TableCell sx={{ color: "white" }}>Allocation as % of Deal Size (Weighted)</TableCell>
-                    <TableCell sx={{ color: "white" }}>Allocation as % of IOI (Weighted)</TableCell>
-                    <TableCell sx={{ color: "white", borderLeft: "2px solid #484547" }}>Monashee Actual Allocation PnL(Gross $)</TableCell>
-                    <TableCell sx={{ color: "white" }}>Model PnL With Actual Allocation(Gross $)</TableCell>
-                    <TableCell sx={{ color: "white" }}>{selectedType === "IPO" ? "Model PnL with model Allocation(0.5%)" : "Model PnL with model Allocation(1%)"}</TableCell>
-                    <TableCell sx={{ color: "white", borderLeft: "2px solid #484547" }}>Monashee Actual AM PnL(Gross $)</TableCell>
-                    <TableCell sx={{ color: "white" }}>Model PnL with model AM(Gross $)</TableCell>
-                    <TableCell sx={{ color: "white", borderLeft: "2px solid #484547" }}>Monashee Actual Total PnL(Gross $)</TableCell>
-                    <TableCell sx={{ color: "white" }}>Model Actual Total PnL(Gross $)</TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      Deal Volume ($)
+                    </TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      Allocation as % of Deal Size (Weighted)
+                    </TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      Allocation as % of IOI (Weighted)
+                    </TableCell>
+                    <TableCell
+                      sx={{ color: "white", borderLeft: "2px solid #484547" }}
+                    >
+                      Monashee Actual Allocation PnL(Gross $)
+                    </TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      Model PnL With Actual Allocation(Gross $)
+                    </TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      {selectedType === "IPO"
+                        ? "Model PnL with model Allocation(0.5%)"
+                        : "Model PnL with model Allocation(1%)"}
+                    </TableCell>
+                    <TableCell
+                      sx={{ color: "white", borderLeft: "2px solid #484547" }}
+                    >
+                      Monashee Actual AM PnL(Gross $)
+                    </TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      Model PnL with model AM(Gross $)
+                    </TableCell>
+                    <TableCell
+                      sx={{ color: "white", borderLeft: "2px solid #484547" }}
+                    >
+                      Monashee Actual Total PnL(Gross $)
+                    </TableCell>
+                    <TableCell sx={{ color: "white" }}>
+                      Model Actual Total PnL(Gross $)
+                    </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -93,20 +173,52 @@ const DealTypeComponent: React.FC<{ data: Data }> = ({ data = {} }) => {
                     const values = tableData[category] || {};
                     const isSummary = category === "Summary";
                     return (
-                      <TableRow key={category} sx={isSummary ? { backgroundColor: "#b5e59c" } : {}}>
+                      <TableRow
+                        key={category}
+                        sx={isSummary ? { backgroundColor: "#b5e59c" } : {}}
+                      >
                         <TableCell>{isSummary ? "" : index + 1}</TableCell>
                         <TableCell>{category}</TableCell>
                         <TableCell>{values["Number of deals"] || 0}</TableCell>
-                        <TableCell>{formatValue(values["Deal volume"] || 0)}</TableCell>
-                        <TableCell>{(values["Weighted Allocation as % of Deal Size"]?.toFixed(2) || "0.00") + "%"}</TableCell>
-                        <TableCell>{(values["Weighted Allocation as % of IOI"]?.toFixed(2) || "0.00") + "%"}</TableCell>
-                        <TableCell sx={{ borderLeft: "2px solid #484547" }}>{formatValue(values["Allocation Return"] || 0)}</TableCell>
-                        <TableCell>{formatValue(values["Model Actual Return"] || 0)}</TableCell>
-                        <TableCell>{formatValue(values["Model Return 1% Allocation"] || 0)}</TableCell>
-                        <TableCell sx={{ borderLeft: "2px solid #484547" }}>{formatValue(values["AM Return"] || 0)}</TableCell>
-                        <TableCell>{formatValue(values["Model AM Return"] || 0)}</TableCell>
-                        <TableCell sx={{ borderLeft: "2px solid #484547" }}>{formatValue(values["Total Return"] || 0)}</TableCell>
-                        <TableCell>{formatValue((values["Model Return 1% Allocation"] || 0) + (values["Model AM Return"] || 0))}</TableCell>
+                        <TableCell>
+                          {formatValue(values["Deal volume"] || 0)}
+                        </TableCell>
+                        <TableCell>
+                          {(values[
+                            "Weighted Allocation as % of Deal Size"
+                          ]?.toFixed(2) || "0.00") + "%"}
+                        </TableCell>
+                        <TableCell>
+                          {(values["Weighted Allocation as % of IOI"]?.toFixed(
+                            2
+                          ) || "0.00") + "%"}
+                        </TableCell>
+                        <TableCell sx={{ borderLeft: "2px solid #484547" }}>
+                          {formatValue(values["Allocation Return"] || 0)}
+                        </TableCell>
+                        <TableCell>
+                          {formatValue(values["Model Actual Return"] || 0)}
+                        </TableCell>
+                        <TableCell>
+                          {formatValue(
+                            values["Model Return 1% Allocation"] || 0
+                          )}
+                        </TableCell>
+                        <TableCell sx={{ borderLeft: "2px solid #484547" }}>
+                          {formatValue(values["AM Return"] || 0)}
+                        </TableCell>
+                        <TableCell>
+                          {formatValue(values["Model AM Return"] || 0)}
+                        </TableCell>
+                        <TableCell sx={{ borderLeft: "2px solid #484547" }}>
+                          {formatValue(values["Total Return"] || 0)}
+                        </TableCell>
+                        <TableCell>
+                          {formatValue(
+                            (values["Model Return 1% Allocation"] || 0) +
+                              (values["Model AM Return"] || 0)
+                          )}
+                        </TableCell>
                       </TableRow>
                     );
                   })}
