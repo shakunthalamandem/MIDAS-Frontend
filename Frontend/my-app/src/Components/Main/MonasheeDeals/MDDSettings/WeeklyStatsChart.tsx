8e39054a857ababd17326888b0f12a08035726af
BAkhileswarreddy <akhileswar.b@goldenhillsindia.com>
2025-02-10 16:19:10 +0530

    final commit

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/WeeklyStatsChart.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/WeeklyStatsChart.tsx
@@ -117,16 +117,18 @@ const WeeklyStatsChart: React.FC = () => {
                 <>
                   <Line type="monotone" dataKey="2024" stroke="#ff7300" name="2024" />
                   <Line type="monotone" dataKey="Average" stroke="#002060" name="Avg(2022, 2023, 2024)" strokeWidth={2} />
+                  <Bar dataKey="2025" barSize={20} fill="#247B5B" name="2025" />
                 </>
               ) : (
                 <>
                   <Line type="monotone" dataKey="2024 Size" stroke="#8a009a" name="2024" />
                   <Line type="monotone" dataKey="Average Size" stroke="#002060" name="Avg(2022, 2023, 2024)" strokeWidth={2} />
+                  <Bar dataKey="2025 Size" barSize={20} fill="#247B5B" name="2025" />
                 </>
               )}
 
               {/* Bar chart for 2025 */}
-              <Bar dataKey="2025" barSize={20} fill="#247B5B" name="2025" />
+              
             </ComposedChart>
           </ResponsiveContainer>
 
