c6e25910d622e34420d12f13d57cb1718db43cfd
vasanthi.g <vasanthi.g@goldenhillsindia.com>
2025-02-11 18:26:28 +0530

    changes done in Cumulative Deals Data

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/WeeklyStatsChart.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/WeeklyStatsChart.tsx
@@ -82,19 +82,29 @@ const WeeklyStatsChart: React.FC = () => {
     );
   }
 
-  const chartData = Object.keys(data["2022"] || {}).map((week) => {
-    return {
-      name: week,
-      "2024": data["2024"]?.[week]?.cumulative_count,
-      "2025": data["2025"]?.[week]?.cumulative_count,
-      Average: data["average"]?.[week]?.cumulative_count,
-
-      "2024 Size": data["2024"]?.[week]?.cumulative_deal_volume,
-      "2025 Size": data["2025"]?.[week]?.cumulative_deal_volume,
-      "Average Size": data["average"]?.[week]?.cumulative_deal_volume,
-    };
-  });
-
+  const highestWeek2025 = Object.keys(data["2025"] || {}).reduce((highest, week) => {
+    const cumulativeCount2025 = data["2025"]?.[week]?.cumulative_count || 0;
+    return cumulativeCount2025 > (data["2025"]?.[highest]?.cumulative_count || 0) ? week : highest;
+  }, "");
+  
+  const chartData = Object.keys(data["2022"] || {})
+    .filter((week) => {
+      // Only include weeks up to the highest week in 2025
+      return week <= highestWeek2025;
+    })
+    .map((week) => {
+      return {
+        name: week,
+        "2024": data["2024"]?.[week]?.cumulative_count,
+        "2025": data["2025"]?.[week]?.cumulative_count,
+        Average: data["average"]?.[week]?.cumulative_count,
+  
+        "2024 Size": data["2024"]?.[week]?.cumulative_deal_volume,
+        "2025 Size": data["2025"]?.[week]?.cumulative_deal_volume,
+        "Average Size": data["average"]?.[week]?.cumulative_deal_volume,
+      };
+    });
+  
   return (
     <Container maxWidth="lg">
       <Card sx={{ boxShadow: 3, marginTop: 8, marginBottom: 10 }}>
