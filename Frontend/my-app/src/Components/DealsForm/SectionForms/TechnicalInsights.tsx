ac273f4171fc656da6f83a095aaf480222576000
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-18 18:21:39 +0530

    added

--- Frontend/my-app/src/Components/DealsForm/SectionForms/TechnicalInsights.tsx
+++ Frontend/my-app/src/Components/DealsForm/SectionForms/TechnicalInsights.tsx
@@ -72,107 +72,107 @@ const TechnicalInsights: React.FC<TechnicalInsightsProps> = ({ data }) => {
           </TableHead>
           <TableBody>
             <TableRow>
-              <TableCell><strong>Discount Analysis V1 (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Discount Analysis V1 (%)</strong></TableCell>
               <TableCell>{data.discount_analysis_v1_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Discount Analysis V2 (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Discount Analysis V2 (%)</strong></TableCell>
               <TableCell>{data.discount_analysis_v2_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Demand Analysis (USD)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Demand Analysis (USD)</strong></TableCell>
               <TableCell>${data.demand_analysis_usd.toFixed(2)}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>LTM FCF Yield</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>LTM FCF Yield</strong></TableCell>
               <TableCell>{data.ltm_fcf_yield}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>LTM Dividend Yield</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>LTM Dividend Yield</strong></TableCell>
               <TableCell>{data.ltm_dividend_yield}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Deal Rating V2</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Deal Rating V2</strong></TableCell>
               <TableCell>{data.deal_rating_v2}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Block Seasoned</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Block Seasoned</strong></TableCell>
               <TableCell>{data.block_seasoned}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Timing Expected</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Timing Expected</strong></TableCell>
               <TableCell>{data.timing_expected}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Clean Up</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Clean Up</strong></TableCell>
               <TableCell>{data.clean_up}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Primary</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Primary</strong></TableCell>
               <TableCell>{data.primary}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Good Register</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Good Register</strong></TableCell>
               <TableCell>{data.good_register}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Positive Liquidity Event</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Positive Liquidity Event</strong></TableCell>
               <TableCell>{data.positive_liquidity_event}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Bank Analyst Coverage</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Bank Analyst Coverage</strong></TableCell>
               <TableCell>{data.bank_analyst_coverage}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Developed Market</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Developed Market</strong></TableCell>
               <TableCell>{data.developed_market}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Deal Wallcrossed</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Deal Wallcrossed</strong></TableCell>
               <TableCell>{data.deal_wallcrossed}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Wallcrossing Coverage (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Wallcrossing Coverage (%)</strong></TableCell>
               <TableCell>{data.wallcrossing_coverage_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Launched with Reference to Market</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Launched with Reference to Market</strong></TableCell>
               <TableCell>{data.launched_with_reference_to_market}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Coverage Estimate (x)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Coverage Estimate (x)</strong></TableCell>
               <TableCell>{data.coverage_estimate_x}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Coverage Speed (mins)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Coverage Speed (mins)</strong></TableCell>
               <TableCell>{data.coverage_speed_mins}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>LO Allocation (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>LO Allocation (%)</strong></TableCell>
               <TableCell>{data.lo_allocation_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Outsized Anchor</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Outsized Anchor</strong></TableCell>
               <TableCell>{data.outsized_anchor}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Upsized</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Upsized</strong></TableCell>
               <TableCell>{data.upsized}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Price vs V1 Model Minus (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Price vs V1 Model Minus (%)</strong></TableCell>
               <TableCell>{data.price_vs_v1_model_minus_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Price vs V1 Model Divide (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Price vs V1 Model Divide (%)</strong></TableCell>
               <TableCell>{data.price_vs_v1_model_divide_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Price vs V2 Model Minus (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Price vs V2 Model Minus (%)</strong></TableCell>
               <TableCell>{data.price_vs_v2_model_minus_percent}%</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell><strong>Price vs V2 Model Divide (%)</strong></TableCell>
+              <TableCell sx={{color:'#4d4d4d'}}><strong>Price vs V2 Model Divide (%)</strong></TableCell>
               <TableCell>{data.price_vs_v2_model_divide_percent}%</TableCell>
             </TableRow>
           </TableBody>
