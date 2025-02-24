6bba3df14bf54598c6fb42ca229ebe74f4004705
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-24 10:14:09 +0530

    added

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/WeeklyDealTable.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/WeeklyDealTable.tsx
@@ -57,7 +57,7 @@ const WeeklyDealTable: React.FC<WeeklyDealTableProps> = ({ data, selectedRegions
     <Table size="small">
       <TableHead>
         <TableRow sx={{ backgroundColor: "#002060" }}>
-          {["Region", "Deal Type", "Count", "Volume", "Allocation Capital", "Allocation Deal %", "Monashee Actual Total PnL(Gross)", "Model Actual Total PnL(Gross)", "Gap"].map((heading) => (
+          {["Region", "Deal Type", "Count", "Volume", "Allocation Capital", "Allocation of Deal Size %", "Monashee Actual Total PnL(Gross)", "Model Actual Total PnL(Gross)", "Gap"].map((heading) => (
             <TableCell key={heading} sx={{ color: "white", minWidth: "40px" }}>
               {heading}
             </TableCell>
