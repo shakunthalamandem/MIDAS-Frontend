6b561510b5658e7ce79a0107315fe246e3182db7
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-27 10:58:34 +0530

    basic setuo is done

---
+++ Frontend/my-app/src/Components/Main/PortfolioAttribution/AttributionMain.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AttributionMain = () => {
+  return (
+    <div>AttributionMain</div>
+  )
+}
+
+export default AttributionMain
---
+++ Frontend/my-app/src/Components/Main/PortfolioAttribution/FundwiseTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FundwiseTable = () => {
+  return (
+    <div>FundwiseTable</div>
+  )
+}
+
+export default FundwiseTable
