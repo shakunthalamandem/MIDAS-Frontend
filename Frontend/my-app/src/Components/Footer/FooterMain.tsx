7f17367a1e820de634ba8d4aae273c827e7adb54
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-02-28 17:06:11 +0530

    added the data version

--- Frontend/my-app/src/Components/Footer/FooterMain.tsx
+++ Frontend/my-app/src/Components/Footer/FooterMain.tsx
@@ -71,10 +71,10 @@ const FooterMain: React.FC = () => {
         
       </Box>
       <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
-            Version 1.04 - Updated February 24, 2025
+            Version 1.05 - Updated February 28, 2025
           </Typography>
           <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
-            Data as of February 21 , 2025
+            Data as of February 27 , 2025
           </Typography>
       <Box
         sx={{
