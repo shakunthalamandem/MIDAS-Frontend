0d3980200d7064e37fc440b2c58b6d5c1145ea6e
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-03-03 16:01:21 +0530

    added

--- Frontend/my-app/src/Components/Main/PortfolioAttribution/FundwiseTable.tsx
+++ Frontend/my-app/src/Components/Main/PortfolioAttribution/FundwiseTable.tsx
@@ -38,15 +38,13 @@ const formatNumber = (value: number) => {
   const isNegative = value < 0;
   const absValue = Math.abs(value);
   let formattedValue;
-  if (absValue >= 1_000_000_000) {
-    formattedValue = (absValue / 1_000_000_000).toFixed(1) + "B";
-  } else if (absValue >= 1_000_000) {
-    formattedValue = (absValue / 1_000_000).toFixed(1) + "M";
-  } else if (absValue >= 1_000) {
-    formattedValue = (absValue / 1_000).toFixed(0) + "K";
+
+  if (absValue >= 1_000) {
+      formattedValue = (absValue / 1_000).toFixed(0) + "K";
   } else {
-    formattedValue = absValue.toFixed(2);
+      formattedValue = absValue.toFixed(2);
   }
+
   return isNegative ? `-$${formattedValue}` : `$${formattedValue}`;
 };
 
