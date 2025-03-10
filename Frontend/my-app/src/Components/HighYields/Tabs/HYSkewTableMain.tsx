a6c07fee7296dfa167ddfd62c9e111c85cafe95e
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-03-10 17:34:03 +0530

    added

--- Frontend/my-app/src/Components/HighYields/Tabs/HYSkewTableMain.tsx
+++ Frontend/my-app/src/Components/HighYields/Tabs/HYSkewTableMain.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import HySectorBasedTable from '../HySkewTables/HySectorBasedTable'
-import HyYearlyBasedTable from '../HySkewTables/HyYearlyBasedTable'
-import HyRatingBasedTable from '../HySkewTables/HyRatingBasedTable'
-import SectorBasedTable from '../../Main/MonasheeGraphs/SkewTables/SectorBasedTable'
-import YearlyBasedTable from '../../Main/MonasheeGraphs/SkewTables/YearlyBasedTable'
+import React from "react";
+import HySectorBasedTable from "../HySkewTables/HySectorBasedTable";
+import HyYearlyBasedTable from "../HySkewTables/HyYearlyBasedTable";
+import HyRatingBasedTable from "../HySkewTables/HyRatingBasedTable";
+import SectorBasedTable from "../../Main/MonasheeGraphs/SkewTables/SectorBasedTable";
+import YearlyBasedTable from "../../Main/MonasheeGraphs/SkewTables/YearlyBasedTable";
 
 const HYSkewTableMain = () => {
   return (
     <>
-    
-    <HyRatingBasedTable   />
-    <HySectorBasedTable   />
-    <HyYearlyBasedTable   />
-    </>
-  )
-}
+      <HySectorBasedTable />
 
-export default HYSkewTableMain
+      <HyYearlyBasedTable />
+      <HyRatingBasedTable />
+    </>
+  );
+};
+
+export default HYSkewTableMain;
