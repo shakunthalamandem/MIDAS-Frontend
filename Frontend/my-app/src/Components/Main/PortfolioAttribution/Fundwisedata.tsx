c8d51b822ed883ca76a7fdd7b722a63834fa303b
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-03-10 18:21:53 +0530

    added

--- Frontend/my-app/src/Components/Main/PortfolioAttribution/Fundwisedata.tsx
+++ Frontend/my-app/src/Components/Main/PortfolioAttribution/Fundwisedata.tsx
@@ -75,7 +75,7 @@ const Fundwisedata: React.FC = () => {
             <Container>
                 {/* Heading Section */}
                 <Typography variant="h5" color="#002060" align="center" fontWeight={600} marginBottom={2}>
-                    2025 YTD Net of Hedge P&L Attribution by Fund a
+                    2025 YTD Net of Hedge P&L Attribution by Fund 
                 </Typography>
 
                 {/* Loading & Error Handling */}
