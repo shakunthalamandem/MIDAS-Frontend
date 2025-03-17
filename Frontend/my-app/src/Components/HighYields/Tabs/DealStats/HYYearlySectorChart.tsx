5d50438a65e8076a4494e7d8f48b56820a33a9b9
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-03-17 11:23:12 +0530

    added

--- Frontend/my-app/src/Components/HighYields/Tabs/DealStats/HYYearlySectorChart.tsx
+++ Frontend/my-app/src/Components/HighYields/Tabs/DealStats/HYYearlySectorChart.tsx
@@ -23,8 +23,9 @@ const HYYearlySectorChart: React.FC<LineChartProps> = ({ data, selectedMetric, c
     return yearData;
   });
 
-  const allSectors = Object.keys(data[Object.keys(data)[0]] || {});
-
+  const allSectors = Array.from(
+    new Set(Object.values(data).flatMap((sectors) => Object.keys(sectors)))
+  );
   const handleCheckboxChange = (sector: string) => {
     setVisibleSectors((prev) =>
       prev.includes(sector) ? prev.filter((item) => item !== sector) : [...prev, sector]
