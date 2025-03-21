487afdebacf8bc88b941f885cac79fbd5d433a3b
vasanthi.g <vasanthi.g@goldenhillsindia.com>
2025-03-21 16:29:32 +0530

    added

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DetailedGapDataTable.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MDDSettings/DetailedGapDataTable.tsx
@@ -68,7 +68,7 @@ const DetailedGapDataTable: React.FC<DetailedGapDataTableProps> = ({ data }) => 
       allocation_deal_size_percentage: row.allocation_deal_size_percentage ? `${row.allocation_deal_size_percentage.toFixed(2)}%` : "0%",
       model_actual_total: row.model_actual_total ? `${formatDealSize(row.model_actual_total.toFixed())}` : "$0",
       monahsee_actual_total: row.monahsee_actual_total ? `${formatDealSize(row.monahsee_actual_total.toFixed())}` : "$0",
-      
+      model_actual_am_return: row.model_actual_am_return ? `${formatDealSize(row.model_actual_am_return.toFixed())}` : "$0",
       model_allocation_gap: row.model_allocation_gap ? `${formatDealSize(row.model_allocation_gap.toFixed())}` : "$0",
       monashee_exit_gap: row.monashee_exit_gap ? `${formatDealSize(row.monashee_exit_gap.toFixed())}` : "$0",
       am_gap: row.am_gap ? `${formatDealSize(row.am_gap.toFixed())}` : "$0",
@@ -235,6 +235,13 @@ const DetailedGapDataTable: React.FC<DetailedGapDataTableProps> = ({ data }) => 
 { 
   field: "am_return", 
   headerName: "Monashee Actual AM PnL(Gross)", 
+  width: 215,
+  renderCell: (params) => `${params.value}`,
+  sortComparator: (v1, v2) => cleanDealSize(v1) - cleanDealSize(v2),
+},
+{ 
+  field: "model_actual_am_return", 
+  headerName: "Model PnL with Actual AM", 
   width: 215,
   renderCell: (params) => `${params.value}`,
   sortComparator: (v1, v2) => cleanDealSize(v1) - cleanDealSize(v2),
