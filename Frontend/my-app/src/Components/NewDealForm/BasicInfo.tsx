e49342887109156ec22a1de65941a8f26d6e8d42
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-05-06 10:58:53 +0530

    added the ui Changes

--- Frontend/my-app/src/Components/NewDealForm/BasicInfo.tsx
+++ Frontend/my-app/src/Components/NewDealForm/BasicInfo.tsx
@@ -53,10 +53,10 @@ const tableLeft1: Row[] = [
   { label: "Deal Captain", key: "deal_captain", type: "select", options: ['Robin','Tom','Block', 'HC', 'Jay','Others'] },
   { label: "Lead Bank", key: "invitation_bank" },
   { label: "Sponsors (Y/N)", key: "sponsor", type: "select", options: ["Y", "N"] },
-  { label: "Primary %", key: "percentage_primary", type: "number" },
 ];
 
 const tableRight1: Row[] = [
+  { label: "Primary %", key: "percentage_primary", type: "number" },
   { label: "Price (Local Currency)", key: "price_local_currency", type: "number" },
   { label: "Discount %", key: "discount_percentage", type: "number" },
   { label: "Last Close Price", key: "last_close_price", type: "number" },
@@ -86,10 +86,12 @@ const tableLeft2: Row[] = [
   { label: "Percent Change Last 7 Days", key: "percent_change_last_7_days" },
   { label: "52 Week High", key: "week_52_high", type: "number" },
   { label: "Percent Below 52 Week High", key: "percent_below_52_week_high" },
-  { label: "3M ADTV (EU) USD", key: "three_month_adtv_eu_usd" },
+
+  
 ];
 
 const tableRight2: Row[] = [
+  { label: "3M ADTV (EU) USD", key: "three_month_adtv_eu_usd" },
   { label: "3M ADTV (EU) Shares", key: "three_month_adtv_eu_shares" },
   { label: "3M ADTV (Local) USD", key: "three_month_adtv_local_usd" },
   { label: "3M ADTV (Local) Shares", key: "three_month_adtv_local_shares" },
@@ -113,10 +115,10 @@ const tableLeft3: Row[] = [
   { label: "Hedge Funds Allocation (%)", key: "hedge_funds_allocation_percent", type: "number" },
   { label: "Local Allocation (%)", key: "local_allocation_percent", type: "number" },
   { label: "International Allocation (%)", key: "international_allocation_percent", type: "number" },
-  { label: "Top 10 Allocation Concentration (%)", key: "top_10_allocation_concentration_percent", type: "number" },
 ];
 
 const tableRight3: Row[] = [
+  { label: "Top 10 Allocation Concentration (%)", key: "top_10_allocation_concentration_percent", type: "number" },
   { label: "Aftermarket Order", key: "aftermarket_order" },
   { label: "Aftermarket Strategy", key: "aftermarket_strategy" },
   { label: "Target Price (Local)", key: "target_price_local", type: "number" },
