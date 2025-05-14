45ecc912243122b2b953156cc9b0200bf809ceb2
shakunthalamandem <shakunthala.m@goldenhillsindia.com>
2025-05-14 10:10:36 +0530

    done

--- Frontend/my-app/src/Components/NewDealForm/DealformInformation.tsx
+++ Frontend/my-app/src/Components/NewDealForm/DealformInformation.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'; // 👈 Import this
+import { useLocation } from 'react-router-dom'; 
 import {
   Box,
   Container,
