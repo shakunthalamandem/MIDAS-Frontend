b280896781d608f0a31725cfe207c58cd2e0f615
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-05-19 16:02:28 +0530

    added the basic changes

--- Frontend/my-app/src/Components/NewDealForm/BasicInfo.tsx
+++ Frontend/my-app/src/Components/NewDealForm/BasicInfo.tsx
@@ -515,9 +515,7 @@ const BasicInfo: React.FC = () => {
     }
   };
 
-
-
-   return (
+  return (
     <Container maxWidth="lg" sx={{ padding: 0, marginBottom: 2 }}>
       {/* Sticky Tabs */}
       <Box
@@ -581,7 +579,7 @@ const BasicInfo: React.FC = () => {
               backgroundColor: "#00732d",
               textTransform: "none",
               borderRadius: 2,
-                  fontWeight: "bold",
+              fontWeight: "bold",
               mr: 2,
               "&:hover": { backgroundColor: "#001540" },
             }}
@@ -604,7 +602,7 @@ const BasicInfo: React.FC = () => {
               color: "#d10000",
               fontWeight: "bold",
               borderRadius: 2,
-              "&:hover": { backgroundColor: "#002060",color: "#ffffff" },
+              "&:hover": { backgroundColor: "#002060", color: "#ffffff" },
             }}
           >
             Reset
@@ -658,7 +656,7 @@ const BasicInfo: React.FC = () => {
                         onChange={handleSelectChange}
                       >
                         {row.options?.map((opt) => (
-                          <MenuItem key={opt} value={opt}>
+                          <MenuItem key={opt} value={opt} >
                             {opt}
                           </MenuItem>
                         ))}
@@ -675,7 +673,18 @@ const BasicInfo: React.FC = () => {
                       value={formData[row.key as keyof typeof formData]}
                       onChange={handleChange}
                       InputLabelProps={{
-                        shrink: true, // Ensures label does not overlap with the date
+                        shrink: true,
+                        sx: { fontSize: "18px", color: "#6501c4" },
+                      }}
+                      InputProps={{
+                        sx: { fontSize: "14px" },
+                      }}
+                      SelectProps={{
+                        MenuProps: {
+                          PaperProps: {
+                            sx: { fontSize: "14px", maxHeight: "300px" },
+                          },
+                        },
                       }}
                     />
                   ) : (
@@ -688,6 +697,19 @@ const BasicInfo: React.FC = () => {
                       value={formData[row.key as keyof typeof formData]}
                       onChange={handleChange}
                       type={row.type}
+                      InputLabelProps={{
+                        sx: { fontSize: "14px", color: "#6501c4" },
+                      }}
+                      InputProps={{
+                        sx: { fontSize: "14px" },
+                      }}
+                      SelectProps={{
+                        MenuProps: {
+                          PaperProps: {
+                            sx: { fontSize: "14px", maxHeight: "300px" },
+                          },
+                        },
+                      }}
                     />
                   )}
                 </Box>
@@ -741,6 +763,7 @@ const BasicInfo: React.FC = () => {
                         name={row.key}
                         value={formData[row.key as keyof typeof formData]}
                         onChange={handleSelectChange}
+                        sx={{maxHeight: "300px"}}
                       >
                         {row.options?.map((opt) => (
                           <MenuItem key={opt} value={opt}>
@@ -761,6 +784,17 @@ const BasicInfo: React.FC = () => {
                       onChange={handleChange}
                       InputLabelProps={{
                         shrink: true,
+                        sx: { fontSize: "18px", color: "#6501c4" },
+                      }}
+                      InputProps={{
+                        sx: { fontSize: "14px" },
+                      }}
+                      SelectProps={{
+                        MenuProps: {
+                          PaperProps: {
+                            sx: { fontSize: "14px", maxHeight: "300px" },
+                          },
+                        },
                       }}
                     />
                   ) : (
@@ -773,6 +807,20 @@ const BasicInfo: React.FC = () => {
                       value={formData[row.key as keyof typeof formData]}
                       onChange={handleChange}
                       type={row.type}
+                      InputLabelProps={{
+                        shrink: true,
+                        sx: { fontSize: "18px", color: "#6501c4" },
+                      }}
+                      InputProps={{
+                        sx: { fontSize: "14px" },
+                      }}
+                      SelectProps={{
+                        MenuProps: {
+                          PaperProps: {
+                            sx: { fontSize: "14px", maxHeight: "300px" },
+                          },
+                        },
+                      }}
                     />
                   )}
                 </Box>
@@ -814,8 +862,8 @@ const BasicInfo: React.FC = () => {
                 gap: 2,
                 borderRadius: 2,
                 p: 2,
-                backgroundColor: "#e6f2ff", 
-                width: "100%", 
+                backgroundColor: "#e6f2ff",
+                width: "100%",
               }}
             >
               {[...tableLeft3, ...tableRight3].map((row) => (
@@ -854,6 +902,20 @@ const BasicInfo: React.FC = () => {
                       value={formData[row.key as keyof typeof formData]}
                       onChange={handleChange}
                       type={row.type}
+                      InputLabelProps={{
+                        shrink: true,
+                        sx: { fontSize: "18px", color: "#6501c4" },
+                      }}
+                      InputProps={{
+                        sx: { fontSize: "14px" },
+                      }}
+                      SelectProps={{
+                        MenuProps: {
+                          PaperProps: {
+                            sx: { fontSize: "14px", maxHeight: "300px" },
+                          },
+                        },
+                      }}
                     />
                   )}
                 </Box>
