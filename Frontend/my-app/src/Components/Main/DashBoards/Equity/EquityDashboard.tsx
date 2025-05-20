54bcfeb949010d971d58a313661b7b9d43df5258
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-05-20 17:46:40 +0530

    addded the basic

---
+++ Frontend/my-app/src/Components/Main/DashBoards/Equity/EquityDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EquityDashboard = () => {
+  return (
+    <div>EquityDashboard</div>
+  )
+}
+
+export default EquityDashboard
---
+++ Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingPageMain = () => {
+  return (
+    <div>LandingPageMain</div>
+  )
+}
+
+export default LandingPageMain
