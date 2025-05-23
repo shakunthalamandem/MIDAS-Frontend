71f7d91620eab5d3d5b4d5d98e362d8273c29ca4
thraunsai08 <tharunsai.k@goldenhillsindia.com>
2025-05-23 12:36:24 +0530

    basic setup done

--- Frontend/my-app/src/Components/Main/DashBoards/Equity/EquityDashboard.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/Equity/EquityDashboard.tsx
@@ -3,10 +3,14 @@ import { Box } from '@mui/material'; // ✅ This was missing
 import SectorwiseTable from './SectorwiseTable';
 import QuarterlyDealsTable from './QuarterlyDealsTable';
 import RegionWiseTable from './RegionWiseTable';
+import NumerSummary from './NumerSummary';
 
 const SideBySideView = () => {
   return (
     <>
+          <Box sx={{ padding: 2 }}>
+        <NumerSummary />
+      </Box>
       <Box sx={{  gap: 2, padding: 2 ,width:'80%'}}>
         <Box sx={{ flex: 1 }}>
           <RegionWiseTable />
