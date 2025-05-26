e06f89cc611b639d7aa2d89b996bdbe5d70cd278
thraunsai08 <tharunsai.k@goldenhillsindia.com>
2025-05-26 13:18:24 +0530

    no changes

--- Frontend/my-app/src/Components/Main/DashBoards/Equity/EquityDashboard.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/Equity/EquityDashboard.tsx
@@ -8,21 +8,8 @@ import NumerSummary from './NumerSummary';
 const SideBySideView = () => {
   return (
     <>
-          <Box sx={{ padding: 2 }}>
-        <NumerSummary />
-      </Box>
-      <Box sx={{  gap: 2, padding: 2 ,width:'80%'}}>
-        <Box sx={{ flex: 1 }}>
-          <RegionWiseTable />
-        </Box>
-        <Box sx={{ flex: 1 }}>
-          <QuarterlyDealsTable />
-        </Box>
-      </Box>
 
-      <Box sx={{ padding: 2 }}>
-        <SectorwiseTable />
-      </Box>
+
     </>
   );
 };
--- Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
@@ -3,6 +3,10 @@ import { Grid, Box, Paper } from '@mui/material';
 import EquityDashboard from './Equity/EquityDashboard';
 import InsightsMain from './InsightsAi/InsightsMain';
 import MDDDashboardMain from './MonasheeMDD/MDDDashboardMain';
+import NumerSummary from './Equity/NumerSummary';
+import QuarterlyDealsTable from './Equity/QuarterlyDealsTable';
+import RegionWiseTable from './Equity/RegionWiseTable';
+import SectorwiseTable from './Equity/SectorwiseTable';
 
 
 const LandingPageMain: React.FC = () => {
@@ -10,14 +14,18 @@ const LandingPageMain: React.FC = () => {
     <Grid container height="100vh">
       {/* Left side: 75% */}
       <Grid item xs={9} p={2} sx={{ overflowY: 'auto' }}>
-        <Box display="flex" flexDirection="column" gap={2}>
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <EquityDashboard />
-          </Paper>
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <MDDDashboardMain />
-          </Paper>
-          {/* Add more components here as needed */}
+      <Box sx={{  gap: 2, padding: 2 ,width:'80%'}}>
+        <Box sx={{ flex: 1 }}>
+          <NumerSummary />
+          <QuarterlyDealsTable />
+        </Box>
+        <Box sx={{ flex: 1 }}>
+          <RegionWiseTable />
+        </Box>
+      </Box>
+
+      <Box sx={{ padding: 2 }}>
+        <SectorwiseTable />
         </Box>
       </Grid>
 
