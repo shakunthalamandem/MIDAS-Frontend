b77975ab1685070126828ffd656abdb652426cd5
vasanthi2510 <vasanthi.g@goldenhillsindia.com>
2025-05-28 17:53:49 +0530

    removed the extra spaces for the T+1M

--- Frontend/my-app/src/Components/Main/DashBoards/Equity/RegionWiseTable.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/Equity/RegionWiseTable.tsx
@@ -185,13 +185,13 @@ const RegionWiseTable = () => {
                 Total Deal Volume ($)
               </TableCell>
               <TableCell sx={{ fontWeight: "bold", minWidth: "70px" }}>
-                Opportunity Value (T + 1M Excess)
+                Opportunity Value (T+1M Excess)
               </TableCell>
               <TableCell sx={{ fontWeight: "bold", minWidth: "80px" }}>
                 % of Positively Performing Deals
               </TableCell>
               <TableCell sx={{ fontWeight: "bold", minWidth: "80px" }}>
-                Expected Returns Excess(T + 1M)
+                Expected Returns Excess
               </TableCell>
             </TableRow>
           </TableHead>
