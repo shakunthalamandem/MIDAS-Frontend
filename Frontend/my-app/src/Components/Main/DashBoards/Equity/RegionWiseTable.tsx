09d33ebb13211688946df5f1652bbcb7a5cc5e1b
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-05-28 16:40:07 +0530

    added the $

--- Frontend/my-app/src/Components/Main/DashBoards/Equity/RegionWiseTable.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/Equity/RegionWiseTable.tsx
@@ -33,11 +33,11 @@ const formatNumber = (value: number): string => {
   let formattedValue: string;
 
   if (absValue >= 1e9) {
-    formattedValue = `${(absValue / 1e9).toFixed()}B`;
+    formattedValue = `$${(absValue / 1e9).toFixed()}B`;
   } else if (absValue >= 1e6) {
-    formattedValue = `${(absValue / 1e6).toFixed()}M`;
+    formattedValue = `$${(absValue / 1e6).toFixed()}M`;
   } else if (absValue >= 1e3) {
-    formattedValue = `${(absValue / 1e3).toFixed()}K`;
+    formattedValue = `$${(absValue / 1e3).toFixed()}K`;
   } else {
     formattedValue = absValue.toString();
   }
