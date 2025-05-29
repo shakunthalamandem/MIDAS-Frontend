e3af3f03d19c481e4da4971c65023266f5f1d680
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-05-29 12:24:15 +0530

    aded the settt

--- Frontend/my-app/src/Components/Main/DashBoards/MonasheeMDD/AllocationGraphsMain.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/MonasheeMDD/AllocationGraphsMain.tsx
@@ -209,8 +209,13 @@ const AllocationGraphsMain: React.FC<AllocationGraphsMainProps> = ({ selectedFil
           color: "#004d2a",
         }}
       >
-          <Typography>
-        Weighted Allocation for 2024 and 2025 by Quarterly
+     <Typography
+        variant="h6"
+        color="#004d2a"
+        fontWeight="bold"
+        gutterBottom
+        sx={{ mb: 2 }}
+      >        Weighted Allocation for 2024 and 2025 by Quarterly
       </Typography>
 
       {loading ? (
