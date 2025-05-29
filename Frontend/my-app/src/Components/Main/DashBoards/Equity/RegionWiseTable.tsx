4379332b9853e170de959a4c1ff6c35d1833d34f
vasanthi2510 <vasanthi.g@goldenhillsindia.com>
2025-05-29 11:38:06 +0530

    added the border and onclick for region wise

--- Frontend/my-app/src/Components/Main/DashBoards/Equity/RegionWiseTable.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/Equity/RegionWiseTable.tsx
@@ -45,7 +45,6 @@ const formatNumber = (value: number): string => {
   return value < 0 ? `-${formattedValue}` : formattedValue;
 };
 
-// Helper to get max for each field
 const getMaxValues = (data: { region: string; data: RegionData }[]) => ({
   Total_Deal_Count: Math.max(...data.map((d) => d.data.Total_Deal_Count)),
   Total_Deal_Volume: Math.max(...data.map((d) => d.data.Total_Deal_Volume)),
@@ -61,17 +60,18 @@ const getMaxValues = (data: { region: string; data: RegionData }[]) => ({
 });
 
 const RegionWiseTable = () => {
-  const [ipoData, setIpoData] = useState<
-    { region: string; data: RegionData }[]
-  >([]);
+  const [ipoData, setIpoData] = useState<{ region: string; data: RegionData }[]>(
+    []
+  );
   const [foData, setFoData] = useState<{ region: string; data: RegionData }[]>(
     []
   );
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
   const handleCardClick = () => {
-  window.open("/equity/capital-markets/skew-table", "_blank");
-};
+    window.open("/equity/capital-markets/skew-table", "_blank");
+  };
 
   useEffect(() => {
     const fetchDeals = async () => {
@@ -94,6 +94,7 @@ const RegionWiseTable = () => {
         });
 
         if (!response.ok) throw new Error("Failed to fetch data");
+
         const result: ApiRegionResponse = await response.json();
 
         const processData = (regionwise: { [region: string]: RegionData }) =>
@@ -120,6 +121,7 @@ const RegionWiseTable = () => {
         <CircularProgress />
       </Box>
     );
+
   if (error)
     return (
       <Typography color="error" textAlign="center">
@@ -131,171 +133,118 @@ const RegionWiseTable = () => {
   const foMax = getMaxValues(foData);
 
   const getStyle = (value: number, max: number) =>
-    value === max ? { fontWeight: "bold", backgroundColor: "#ffd9b3" } : {};
+    value === max
+      ? { fontWeight: "bold", backgroundColor: "#ffd9b3", border: "1px solid #ccc" }
+      : { border: "1px solid #ccc" };
 
   return (
     <Box>
-      <TableContainer component={Paper} elevation={4} >
+      <TableContainer component={Paper} elevation={4}>
         <Box
           sx={{
             overflow: "hidden",
             whiteSpace: "nowrap",
             width: "100%",
+            cursor: "pointer",
           }}
+          onClick={handleCardClick}
         >
           <Typography
             variant="body1"
             sx={{
-              // display: "inline-block",
-              // animation: "scroll-left 15s linear infinite",
               fontWeight: "bold",
               color: "#054511",
               textAlign: "center",
               p: 1.5,
+              cursor: "pointer"
             }}
           >
             Region-wise Skew Table - IPO and FO Deals for 2025 (Q1) with{" "}
             <span style={{ color: "red" }}>Top</span> Highlights
           </Typography>
+
         </Box>
 
-        <Table size="small">
+        <Table
+          size="small"
+          sx={{
+            borderCollapse: "collapse",
+            width: "100%",
+          }}
+        >
           <TableHead sx={{ backgroundColor: "#f5f5f5" }}>
             <TableRow>
-              <TableCell sx={{ fontWeight: "bold", minWidth: "110px" }}>
+              <TableCell sx={{ fontWeight: "bold", border: "1px solid #ccc", minWidth: "110px" }}>
                 Region
               </TableCell>
-              <TableCell sx={{ fontWeight: "bold", minWidth: "40px" }}>
+              <TableCell sx={{ fontWeight: "bold", border: "1px solid #ccc", minWidth: "40px" }}>
                 Total Deal Count
               </TableCell>
-              <TableCell sx={{ fontWeight: "bold", minWidth: "60px" }}>
+              <TableCell sx={{ fontWeight: "bold", border: "1px solid #ccc", minWidth: "60px" }}>
                 Total Deal Volume ($)
               </TableCell>
-              <TableCell sx={{ fontWeight: "bold", minWidth: "70px" }}>
+              <TableCell sx={{ fontWeight: "bold", border: "1px solid #ccc", minWidth: "70px" }}>
                 Opportunity Value (T+1M Excess)
               </TableCell>
-              <TableCell sx={{ fontWeight: "bold", minWidth: "80px" }}>
+              <TableCell sx={{ fontWeight: "bold", border: "1px solid #ccc", minWidth: "80px" }}>
                 % of Positively Performing Deals
               </TableCell>
-              <TableCell sx={{ fontWeight: "bold", minWidth: "80px" }}>
+              <TableCell sx={{ fontWeight: "bold", border: "1px solid #ccc", minWidth: "80px" }}>
                 Expected Returns Excess
               </TableCell>
             </TableRow>
           </TableHead>
+
           <TableBody>
             <TableRow>
-              <TableCell
-                colSpan={6}
-                sx={{
-                  fontWeight: "bold",
-                  backgroundColor: "#002060",
-                  color: "white",
-                  textAlign: "center",
-                }}
-              >
+              <TableCell colSpan={6} sx={{ fontWeight: "bold", backgroundColor: "#002060", color: "white", textAlign: "center", border: "1px solid #ccc" }}>
                 IPO
               </TableCell>
             </TableRow>
+
             {ipoData.map((row) => (
               <TableRow key={`IPO-${row.region}`}>
-                <TableCell>{row.region}</TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Total_Deal_Count,
-                    ipoMax.Total_Deal_Count
-                  )}
-                >
+                <TableCell sx={{ border: "1px solid #ccc" }}>{row.region}</TableCell>
+                <TableCell sx={getStyle(row.data.Total_Deal_Count, ipoMax.Total_Deal_Count)}>
                   {row.data.Total_Deal_Count}
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Total_Deal_Volume,
-                    ipoMax.Total_Deal_Volume
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Total_Deal_Volume, ipoMax.Total_Deal_Volume)}>
                   {formatNumber(row.data.Total_Deal_Volume)}
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Long_Opportunity_Value,
-                    ipoMax.Long_Opportunity_Value
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Long_Opportunity_Value, ipoMax.Long_Opportunity_Value)}>
                   {formatNumber(row.data.Long_Opportunity_Value)}
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Positively_Performing_Deals_Percentage,
-                    ipoMax.Positively_Performing_Deals_Percentage
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Positively_Performing_Deals_Percentage, ipoMax.Positively_Performing_Deals_Percentage)}>
                   {row.data.Positively_Performing_Deals_Percentage}%
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Expected_Returns_Excess,
-                    ipoMax.Expected_Returns_Excess
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Expected_Returns_Excess, ipoMax.Expected_Returns_Excess)}>
                   {row.data.Expected_Returns_Excess}%
                 </TableCell>
               </TableRow>
             ))}
+
             <TableRow>
-              <TableCell
-                colSpan={6}
-                sx={{
-                  fontWeight: "bold",
-                  backgroundColor: "#002060",
-                  color: "white",
-                  textAlign: "center",
-                }}
-              >
+              <TableCell colSpan={6} sx={{ fontWeight: "bold", backgroundColor: "#002060", color: "white", textAlign: "center", border: "1px solid #ccc" }}>
                 FO
               </TableCell>
             </TableRow>
+
             {foData.map((row) => (
               <TableRow key={`FO-${row.region}`}>
-                <TableCell>{row.region}</TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Total_Deal_Count,
-                    foMax.Total_Deal_Count
-                  )}
-                >
+                <TableCell sx={{ border: "1px solid #ccc" }}>{row.region}</TableCell>
+                <TableCell sx={getStyle(row.data.Total_Deal_Count, foMax.Total_Deal_Count)}>
                   {row.data.Total_Deal_Count}
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Total_Deal_Volume,
-                    foMax.Total_Deal_Volume
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Total_Deal_Volume, foMax.Total_Deal_Volume)}>
                   {formatNumber(row.data.Total_Deal_Volume)}
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Long_Opportunity_Value,
-                    foMax.Long_Opportunity_Value
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Long_Opportunity_Value, foMax.Long_Opportunity_Value)}>
                   {formatNumber(row.data.Long_Opportunity_Value)}
                 </TableCell>
-                <TableCell
-                  sx={getStyle(
-                    row.data.Positively_Performing_Deals_Percentage,
-                    foMax.Positively_Performing_Deals_Percentage
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Positively_Performing_Deals_Percentage, foMax.Positively_Performing_Deals_Percentage)}>
                   {row.data.Positively_Performing_Deals_Percentage}%
                 </TableCell>
-                <TableCell
-                
-                  sx={getStyle(
-                    row.data.Expected_Returns_Excess,
-                    foMax.Expected_Returns_Excess
-                  )}
-                >
+                <TableCell sx={getStyle(row.data.Expected_Returns_Excess, foMax.Expected_Returns_Excess)}>
                   {row.data.Expected_Returns_Excess}%
                 </TableCell>
               </TableRow>
