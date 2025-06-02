08eeb7c361f822671fe986e2c0220a1a313052b1
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-06-02 18:30:33 +0530

    added

--- Frontend/my-app/src/Components/Main/DashBoards/MonasheeMDD/MDDDashboardMain.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/MonasheeMDD/MDDDashboardMain.tsx
@@ -22,7 +22,7 @@ const MDDDashboardMain = () => {
       <SummaryGraphsGap />
 
         <Typography variant="h5" sx={{  color: '#002060', textAlign: 'center' ,marginBottom:'20px'}}>
-          Todays Deals Dashboard
+          Today Deals Dashboard
         </Typography>
         <Grid item xs={12}>
           <AiDashboard />
