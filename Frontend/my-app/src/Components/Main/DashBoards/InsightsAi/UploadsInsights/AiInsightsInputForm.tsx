77c7f315dd9dc22693d48209b655297c7f466363
shakunthalamandem <shakunthala.m@goldenhillsindia.com>
2025-06-03 14:34:02 +0530

    added teh format

--- Frontend/my-app/src/Components/Main/DashBoards/InsightsAi/UploadsInsights/AiInsightsInputForm.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/InsightsAi/UploadsInsights/AiInsightsInputForm.tsx
@@ -78,19 +78,19 @@ const AiInsightsInputForm: React.FC = () => {
             AI Insights Input Form
           </Typography>
           <Stack spacing={3}>
-      <TextField
-  label="Date"
-  type="date"
-  fullWidth
-  value={date ? date.toISOString().split("T")[0] : ""}
-  onChange={(e) => {
-    const newDate = e.target.value ? new Date(e.target.value) : null;
-    setDate(newDate);
-  }}
-  InputLabelProps={{
-    shrink: true,
-  }}
-/>
+            <TextField
+              label="Date"
+              type="date"
+              fullWidth
+              value={date ? date.toISOString().split("T")[0] : ""}
+              onChange={(e) => {
+                const newDate = e.target.value ? new Date(e.target.value) : null;
+                setDate(newDate);
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
 
 
             <TextField
