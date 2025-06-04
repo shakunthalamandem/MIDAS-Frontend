e4ec9c62730035d82d14f118c5f7d2df67744347
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-06-04 19:36:26 +0530

    added the basic setup

---
+++ Frontend/my-app/src/Components/Main/WriteUpsRecords/CustomAnalysiswriteups.tsx
@@ -0,0 +1,24 @@
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
+
+const CustomAnalysiswriteups = () => {
+  return (
+    <>
+      <Container>
+        <Box padding={4}>
+          <Typography
+            variant="h6"
+            mb={3}
+            color="#002060"
+            fontWeight="bold"
+            align="center"
+          >
+           Custom Analysis Writeups
+          </Typography>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default CustomAnalysiswriteups;
