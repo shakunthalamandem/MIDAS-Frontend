9ae4275d34c345029d5dfa356543377140fa3b70
charangoldenhills <charan.d@goldenhillsindia.com>
2025-06-04 19:18:46 +0530

    changes in gap analysis pop up

--- Frontend/my-app/src/Components/Main/MonasheeDeals/MddGraphs/AllocationCaptureReturn.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeDeals/MddGraphs/AllocationCaptureReturn.tsx
@@ -11,33 +11,7 @@ const AllocationCaptureReturn = () => {
       textAlign="center"
       sx={{ width: "100%", overflow: "hidden", position: "relative" }}
     >
-        <Typography
-    variant="body2"
-    sx={{
-      "& .marquee": {
-        display: "inline-block",
-        whiteSpace: "nowrap",
-        animation: "marquee 40s linear infinite",
-        color: "#666666",
-        fontWeight: "bold",
-        fontStyle: "italic",
-        paddingLeft: "10px",
-        paddingR: "50px",
-        paddingRight: "10px",
-      },
-      "@keyframes marquee": {
-        "0%": { transform: "translateX(100%)" },
-        "100%": { transform: "translateX(-100%)" },
-      },
-      "& .marquee:hover": {
-        animationPlayState: "paused",
-      },
-    }}
-  >
-    <span className="marquee">
-    As for the below GAP Analysis, we have assumed that 0.5% IPO Allocation, 1% for FO Allocation, and 0.5% AM for both IPOs and FOs. There is a Position limit of $30M. Also note that, for each year deals issued in that year are considered, and the EXIT date for actual PnL could be in future years. For Model, the EXIT date is always T+1Month. This analysis includes SPACs and PIPEs. 
-    </span>
-  </Typography>
+        
       <MddMain apiName="gap_analysis" />
     </Box>
   );
