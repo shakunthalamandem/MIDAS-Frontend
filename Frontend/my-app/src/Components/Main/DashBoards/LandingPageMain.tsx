cee92d7900f7fd29b6c5fd244c4b791142b76980
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-06-04 18:58:00 +0530

    ADDED THE COLROS

--- Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
@@ -14,7 +14,7 @@ const LandingPageMain: React.FC = () => {
           <Paper elevation={3} sx={{ p: 2, backgroundColor: '#a3b5e7' }}>
             <EquityDashboard />
           </Paper>
-          <Paper elevation={3} sx={{ p: 2, backgroundColor: '#9ed4c6' }}>
+          <Paper elevation={3} sx={{ p: 2, backgroundColor: '#ecf6bb' }}>
             <MDDDashboardMain />
           </Paper>
         </Box>
