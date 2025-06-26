2623e346700bf73550ba0c5a701fc5a5272e02a6
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-06-26 12:01:45 +0530

    added the region

--- Frontend/my-app/src/Components/Main/MonasheeGraphs/SkewTableMain.tsx
+++ Frontend/my-app/src/Components/Main/MonasheeGraphs/SkewTableMain.tsx
@@ -12,7 +12,7 @@ import {
 import YearlyBasedTable from "./SkewTables/YearlyBasedTable";
 import SectorBasedTable from "./SkewTables/SectorBasedTable";
 import LeadBankTable from "./SkewTables/LeadBankBasedTable";
-import RegionBasedTable from './SkewTables/RegionBasedTable';
+import RegionBasedTable from "./SkewTables/RegionBasedTable";
 
 const SkewTableMain: React.FC = () => {
   const [selectedView, setSelectedView] = useState<string>("sector");
@@ -28,7 +28,7 @@ const SkewTableMain: React.FC = () => {
       case "yearly":
         return <YearlyBasedTable />;
       case "region":
-        return <YearlyBasedTable />;
+        return <RegionBasedTable />;
       case "leadBank":
         return <LeadBankTable />;
       default:
@@ -37,14 +37,75 @@ const SkewTableMain: React.FC = () => {
   };
 
   return (
-  
-    <>
-    <SectorBasedTable   />
-    <YearlyBasedTable   />
-    <LeadBankTable />
+    <Box sx={{ py: 4 }}>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            background: "linear-gradient(to right,rgb(185, 169, 224),rgb(235, 191, 161))",
+            
+            borderRadius: 2,
+            boxShadow: 3,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mb: 4,
+          }}
+        >
+          <FormControl component="fieldset" sx={{ width: "100%" }}>
+            <RadioGroup
+              row
+              value={selectedView}
+              onChange={handleChange}
+              aria-label="skew-table-view"
+              name="skew-table-view"
+              sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                flexWrap: "wrap",
+              }}
+            >
+              {[
+                { value: "sector", label: "Yearly Based" },
+                { value: "yearly", label: "Sector Based" },
+                { value: "region", label: "Region Based" },
+                { value: "leadBank", label: "Lead Bank Based" },
+              ].map((option) => (
+                <FormControlLabel
+                  key={option.value}
+                  value={option.value}
+                  control={
+                    <Radio
+                      sx={{
+                        color: "#005f4c",
+                        "&.Mui-checked": {
+                          color: "#002060",
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography
+                      sx={{
+                        color: "#002060",
+                        fontWeight: "bold",
+                        fontSize: "1rem",
+                      }}
+                    >
+                      {option.label}
+                    </Typography>
+                  }
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </Box>
 
-    </>
-  )
-}
+        <Fade in timeout={500}>
+          <Box>{renderTable()}</Box>
+        </Fade>
+      </Container>
+    </Box>
+  );
+};
 
 export default SkewTableMain;
