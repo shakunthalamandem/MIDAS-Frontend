c6ddaa5df814786dca34ccdf60fb47ad879a1a58
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-07-01 15:19:20 +0530

    added the coors

--- Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/LandingPageMain.tsx
@@ -21,7 +21,7 @@ const LandingPageMain: React.FC = () => {
 
         {/* Right side: 15% */}
         <Box
-          sx={{ width: "15%", p: 2, backgroundColor: "#810c4a", mt: 2, mb: 2 }}
+          sx={{ width: "15%", p: 2, backgroundColor: "#dda1c1", mt: 2, mb: 2 }}
         >
           <InsightsMain />
         </Box>
