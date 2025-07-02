a448c2587aff8e21b2bc18d5088c36ccae278098
ghcit2025 <ghcit@goldenhillsindia.com>
2025-07-02 12:13:01 +0530

    changes done

--- Frontend/my-app/src/Components/Main/DashBoards/IntelligenceDashboard.tsx
+++ Frontend/my-app/src/Components/Main/DashBoards/IntelligenceDashboard.tsx
@@ -46,7 +46,6 @@ const handleFoYearToggle = (year: number) => {
 
   return (
     <>
-      <Container maxWidth="xl">
         <Box display="flex" width="100%" pt={2}>
           <Box display="flex" flexDirection="column" gap={4} width="100%">
 
@@ -256,7 +255,6 @@ const handleFoYearToggle = (year: number) => {
             )}
           </Box>
         </Box>
-      </Container>
     </>
   );
 };
