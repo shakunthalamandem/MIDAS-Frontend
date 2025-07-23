f6c776da31b524dffce20a90fb65f593e7cfb4fa
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-07-23 10:21:13 +0530

    ADDED THE BASUC SETUP

--- Frontend/my-app/src/Components/PNLAttribution/AttributionFundMain/MostAgedStocksTable.tsx
+++ Frontend/my-app/src/Components/PNLAttribution/AttributionFundMain/MostAgedStocksTable.tsx
@@ -72,7 +72,7 @@ const MostAgedStocksTable: React.FC<MostAgedStocksTableProps> = ({ fund }) => {
     { field: "first_trade_date", headerName: "Issue Date", flex: 1 },
     {
       field: "cumulative_pnl",
-      headerName: "Cumulative PnL",
+      headerName: "YTD PnL",
       flex: 1,
       renderCell: ({ value }) => (
         <span style={{ color: value > 0 ? "green" : value < 0 ? "red" : "black" }}>
