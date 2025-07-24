c13d74db0fd52ff3d776eb725764721fcdef3a5d
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-07-24 17:19:37 +0530

    added

--- Frontend/my-app/src/Components/PNLAttribution/types/index.ts
+++ Frontend/my-app/src/Components/PNLAttribution/types/index.ts
@@ -63,7 +63,7 @@ export const formatNumber = (value: number): string => {
 export const dealGridColumns: GridColDef[] = [
   { field: "first_trade_date", headerName: "Trade Date", flex: 1 },
   { field: "client_symbol", headerName: "Ticker", flex: 1 },
-  // { field: "custom_group_2", headerName: "Sector", flex: 1 },
+  { field: "custom_group_2", headerName: "Sector", flex: 1 },
   { field: "fo_type", headerName: "Deal Type", flex: 1 ,  renderCell: (params) => params.value != null ? params.value : "-"
 },
 
