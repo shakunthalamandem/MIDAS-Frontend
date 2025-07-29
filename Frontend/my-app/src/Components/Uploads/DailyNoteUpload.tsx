e5f4a343f6d858f3149e2ed3c533d40fa5ec2ada
shakunthalamandem <shakunthala.m@goldenhillsindia.com>
2025-07-29 12:39:57 +0530

    done

--- Frontend/my-app/src/Components/Uploads/DailyNoteUpload.tsx
+++ Frontend/my-app/src/Components/Uploads/DailyNoteUpload.tsx
@@ -86,7 +86,7 @@ const DailyNoteUpload = () => {
       const apiUrl = process.env.REACT_APP_API_URL;
       const token = localStorage.getItem("access_token");
 
-      const res = await fetch(`${apiUrl}/api/daily_note_upload/`, {
+      const res = await fetch(`${apiUrl}/api/daily_note_deals_upload/`, {
         method: "POST",
         body: formData,
         headers: {
