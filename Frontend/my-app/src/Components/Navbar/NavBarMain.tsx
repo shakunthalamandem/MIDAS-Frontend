4b9586b8c7973bd9224b00bed6f9b757cf689b5b
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-07-31 18:36:35 +0530

    added

--- Frontend/my-app/src/Components/Navbar/NavBarMain.tsx
+++ Frontend/my-app/src/Components/Navbar/NavBarMain.tsx
@@ -262,7 +262,7 @@ const NavbarMain: React.FC = () => {
               label="Macro (Prime)"
               menuItems={[
                 { label: "Prime", path: "/macro/prime" },
-                { label: "News Summary", path: "/macro/news-summary" },
+                // { label: "News Summary", path: "/macro/news-summary" },
                 { label: "Sector", path: "/macro/sector" },
               ]}
               selectedTab={selectedTab}
