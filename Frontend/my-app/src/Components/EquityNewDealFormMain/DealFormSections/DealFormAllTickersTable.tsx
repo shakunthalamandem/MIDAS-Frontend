80d1c5932d5b0d23c8dff7d058e383ea3e18fe01
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-08-13 14:18:07 +0530

    added

--- Frontend/my-app/src/Components/EquityNewDealFormMain/DealFormSections/DealFormAllTickersTable.tsx
+++ Frontend/my-app/src/Components/EquityNewDealFormMain/DealFormSections/DealFormAllTickersTable.tsx
@@ -82,7 +82,7 @@ const DealFormAllTickersTable: React.FC<DealFormAllTickersTableProps> = ({
         </span>
       ),
     },
-    { field: "pricing_date", headerName: "Pricing Date", width: 200 },
+    { field: "pricing_date", headerName: "Pricing Date", width: 180 },
     { field: "deal_type", headerName: "Deal Type", width: 150 },
     { field: "deal_captain", headerName: "Deal Captain", width: 180 },
     {
@@ -136,6 +136,7 @@ const DealFormAllTickersTable: React.FC<DealFormAllTickersTableProps> = ({
         onRowClick={(params) => {
           if (onRowClick) onRowClick(params.row as TickerData);
         }}
+      
       />
     </Box>
     </Container>
