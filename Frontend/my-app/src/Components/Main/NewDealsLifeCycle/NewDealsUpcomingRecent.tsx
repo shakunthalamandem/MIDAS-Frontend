95f33cc30cd46e00e70b7554f11869bad658ec57
vasanthi2510 <vasanthi.g@goldenhillsindia.com>
2025-08-29 16:43:02 +0530

    added the current one

--- Frontend/my-app/src/Components/Main/NewDealsLifeCycle/NewDealsUpcomingRecent.tsx
+++ Frontend/my-app/src/Components/Main/NewDealsLifeCycle/NewDealsUpcomingRecent.tsx
@@ -88,15 +88,10 @@ const token = localStorage.getItem("access_token");
           </Grid>
 
         </Grid>
-<<<<<<< HEAD
         
           <Grid item xs={12}>
             <DealIoiValuesTable data={selectedDeal} />
           </Grid>
-=======
-        <DealIoiValuesTable data={selectedDeal} />
-        
->>>>>>> d6cdc726a3b8e2c142145e5ce0b27d6469269953
         </>
       )}
     </Container>
