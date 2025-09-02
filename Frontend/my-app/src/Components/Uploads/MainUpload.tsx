71a54e7c9f643abc078d327c92fcd18e76a1a7f7
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-09-02 18:52:21 +0530

    rmeoved the data

--- Frontend/my-app/src/Components/Uploads/MainUpload.tsx
+++ Frontend/my-app/src/Components/Uploads/MainUpload.tsx
@@ -60,12 +60,6 @@ const uploadConfigs = [
     buttonColor: "error",
   },
   {
-    key: "FOfinancialForecasts",
-    label: "Upload FO S1 FinancialForecasts",
-    apiEndpoint: "fo_financial_forecasts_data_upload",
-    buttonColor: "success",
-  },
-  {
     key: "focompanymetric",
     label: "Upload FO Companymetric Data",
     apiEndpoint: "fo_companymetric_data_upload",
