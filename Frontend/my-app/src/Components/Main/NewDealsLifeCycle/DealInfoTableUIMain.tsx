e1d6b3969ee6af9f68121a00999e9e4f4524363b
vasanthi2510 <vasanthi.g@goldenhillsindia.com>
2025-09-16 12:38:27 +0530

    added the sorting correctly

--- Frontend/my-app/src/Components/Main/NewDealsLifeCycle/DealInfoTableUIMain.tsx
+++ Frontend/my-app/src/Components/Main/NewDealsLifeCycle/DealInfoTableUIMain.tsx
@@ -40,7 +40,6 @@ const formatDealSize = (dealSize: any) => {
       broad_region: row.broad_region || "N/A",
       deal_type: row.deal_type || "N/A",
       fo_type: row.deal_type === "IPO" ? "-" : row.fo_type || "-",
-      number_of_shares_offered: row.number_of_shares_offered ? `${row.number_of_shares_offered.toFixed(0)}` : "0",
       issue_offer_price: row.issue_offer_price ? formatDealSize(row.issue_offer_price.toFixed(2)) : "$0",
       ioi_deal_size: row.ioi_deal_size ? `${row.ioi_deal_size.toFixed(2)}%` : "0%",
       allocation_return: row.allocation_return ? `${formatDealSize(row.allocation_return.toFixed())}` : "$0",
