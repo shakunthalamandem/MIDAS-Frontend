1633b3d8512a98a31dac24a7d5433498d7731924
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-09-30 11:38:28 +0530

    fixed the issue in the refresh toiekn in the code of ipo and fo details

--- Frontend/my-app/src/Components/IPODashboardLLM/IPODashboardMain/IPOCompsTableMain/Services/FOapi.ts
+++ Frontend/my-app/src/Components/IPODashboardLLM/IPODashboardMain/IPOCompsTableMain/Services/FOapi.ts
@@ -1,16 +1,17 @@
 const apiUrl = process.env.REACT_APP_API_URL!;
-const token = localStorage.getItem("access_token");
 
-const headers = {
-  "Content-Type": "application/json",
-  ...(token && { Authorization: `Bearer ${token}` }),
+const getHeaders = () => {
+  const token = localStorage.getItem("access_token");
+  return {
+    "Content-Type": "application/json",
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+  };
 };
 
 export const searchTickers = async (query: string) => {
-  const res = await fetch(`${apiUrl}/api/factset_tickerlist/?search=${query}`, {  headers: {
-            "Content-Type": "application/json",
-            Authorization: token ? `Bearer ${token}` : "",
-          }, });
+  const res = await fetch(`${apiUrl}/api/factset_tickerlist/?search=${query}`, {
+    headers: getHeaders(),
+  });
   if (!res.ok) throw new Error("Failed to fetch tickers");
   return res.json();
 };
@@ -18,10 +19,7 @@ export const searchTickers = async (query: string) => {
 export const addCompetitor = async (ticker: string, competitor: string) => {
   const res = await fetch(`${apiUrl}/api/fo_fs_ticker_competitor_insert/`, {
     method: "POST",
-     headers: {
-            "Content-Type": "application/json",
-            Authorization: token ? `Bearer ${token}` : "",
-          },
+    headers: getHeaders(),
     body: JSON.stringify({ ticker, competitor }),
   });
   if (!res.ok) throw new Error("No data found for the given ticker");
@@ -31,25 +29,17 @@ export const addCompetitor = async (ticker: string, competitor: string) => {
 export const deleteCompetitor = async (ticker: string, competitor: string) => {
   const res = await fetch(`${apiUrl}/api/fo_fs_ticker_competitor_delete/`, {
     method: "POST",
-     headers: {
-            "Content-Type": "application/json",
-            Authorization: token ? `Bearer ${token}` : "",
-          },
+    headers: getHeaders(),
     body: JSON.stringify({ ticker, competitor }),
   });
   if (!res.ok) throw new Error("Failed to delete competitor");
   return res.json();
 };
 
-
-
 export const updateRow = async (row: any) => {
   const res = await fetch(`${apiUrl}/api/fo_fs_fundamental_data_upload/`, {
     method: "PATCH",
-     headers: {
-            "Content-Type": "application/json",
-            Authorization: token ? `Bearer ${token}` : "",
-          },
+    headers: getHeaders(),
     body: JSON.stringify(row),
   });
   if (!res.ok) throw new Error("Failed to update row");
