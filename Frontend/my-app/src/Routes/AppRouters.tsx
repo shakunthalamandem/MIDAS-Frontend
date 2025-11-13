265935bd86f7f65233b1fc688b6df2bb85efcf7a
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-11-13 12:49:23 +0530

    added the folder structure

--- Frontend/my-app/src/Routes/AppRouters.tsx
+++ Frontend/my-app/src/Routes/AppRouters.tsx
@@ -79,7 +79,7 @@ import DailyNoteDeleteTickersData from "../Components/Uploads/DailyNoteDeleteTic
 import IPOUploadsPage from "../Components/IPOwriteUp/IPOUploadsPage";
 import PNLRiskReportMain from "../Components/PNLAttribution/RiskReportMain/PNLRiskReportMain";
 import ABBModelMain from "../Components/ABBModelMain/ABBModelMain";
-import ABBDiscountDataMainFunction from "../Components/DiscountDataModel/ABBDiscountDataMainFunction";
+import ABBDiscountDataMainFunction from "../Components/ABBModelMain/DiscountDataModel/ABBDiscountDataMainFunction";
 
 const AppRouters: React.FC = () => {
   return (
