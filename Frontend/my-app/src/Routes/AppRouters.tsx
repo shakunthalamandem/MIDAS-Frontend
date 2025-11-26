dd1252fdd3ae5f22ea9745206e03d1d64991b8e8
vasanthi2510 <vasanthi.g@goldenhillsindia.com>
2025-11-26 17:09:29 +0530

    added the tempt charts

--- Frontend/my-app/src/Routes/AppRouters.tsx
+++ Frontend/my-app/src/Routes/AppRouters.tsx
@@ -80,6 +80,7 @@ import IPOUploadsPage from "../Components/IPOwriteUp/IPOUploadsPage";
 import ABBDiscountDataMainFunction from "../Components/ABBModelMain/DiscountDataModel/ABBDiscountDataMainFunction";
 import ABBModelMain from "../Components/ABBModelMain/ABBModelMain";
 import AIMLResultsHome from "../Components/AIMLResults/AIMLResultsHome";
+import DealPricesChart from "../Components/AIMLResults/DealPricesChart";
 
 const AppRouters: React.FC = () => {
   return (
@@ -184,6 +185,7 @@ const AppRouters: React.FC = () => {
           <Route path="/deal_data_upload" element={<UnifiedDealDataUpload />} />
         </Route>    
         <Route path="/equity/ipo_dashboard" element={<AuthGuard><WriteUpIPODashbaord /></AuthGuard>} />
+        <Route path="/prices_chart" element={<AuthGuard><DealPricesChart /></AuthGuard>} />
 
         <Route path="/equity/ipo_dashboard/:ticker" element={<AuthGuard><WriteUpIPODashbaord /></AuthGuard>} />
 
