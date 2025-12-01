5c577b50823f3838f19f91b80b397174702240f2
vasanthi2510 <vasanthi.g@goldenhillsindia.com>
2025-12-01 14:39:56 +0530

    removed the filter

--- Frontend/my-app/src/Components/MonasheeMDRMain/MDRFundRegionWiseFilters.tsx
+++ Frontend/my-app/src/Components/MonasheeMDRMain/MDRFundRegionWiseFilters.tsx
@@ -70,10 +70,6 @@ const MDRFundRegionWiseFilters: React.FC<Props> = ({
       }}
     >
       <CardContent>
-        <Typography variant="subtitle1" sx={{ mb: 2, fontWeight: 600 }}>
-          Filters
-        </Typography>
-
         <Grid container spacing={2}>
           <Grid item xs={12} md={3}>
             <TextField
