51f62f86f307753430d059ab74590b65ebf64a7a
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-12-04 12:57:51 +0530

    added

--- Frontend/my-app/src/Components/MDRMainS3Data/MDRMainTableS3Data.tsx
+++ Frontend/my-app/src/Components/MDRMainS3Data/MDRMainTableS3Data.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { MDRDailyPortfolioContainer } from './MDRDailyPortfolioContainer'
 import RegionWiseMDRTables from './RegionWiseMDRTables'
 import { Typography } from '@mui/material'
+import MDRCummulativeRegionChartMain from './MDRCharts/MDRCummulativeRegionChartMain'
 
 const MDRMainTableS3Data = () => {
   return (
@@ -32,6 +33,7 @@ const MDRMainTableS3Data = () => {
         Welcome to Monashee Daily Report
       </Typography>
       <MDRDailyPortfolioContainer />
+            <MDRCummulativeRegionChartMain />
       <RegionWiseMDRTables />
       
 </>  )
