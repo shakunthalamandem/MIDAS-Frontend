14eda358b123378c250dcf1b8e87817c403fde13
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-12-04 14:54:23 +0530

    added the basic set up of the folders are done

---
+++ Frontend/my-app/src/Components/MDRMainS3Data/MDRRegionWiseTables/MDRRegionWiseTablesDataMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MDRRegionWiseTablesDataMain = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MDRRegionWiseTablesDataMain
---
+++ Frontend/my-app/src/Components/MDRMainS3Data/MDRTopPerformanceDataTables/MDRPerformanceTableMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MDRPerformanceTableMain = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MDRPerformanceTableMain
