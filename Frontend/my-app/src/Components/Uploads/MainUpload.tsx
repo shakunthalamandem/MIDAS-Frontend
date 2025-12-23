aac85a18f70a3d1604b0be9db3555aa851061a3a
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2025-12-23 15:32:14 +0530

    asa

--- Frontend/my-app/src/Components/Uploads/MainUpload.tsx
+++ Frontend/my-app/src/Components/Uploads/MainUpload.tsx
@@ -40,6 +40,7 @@ const monasheeUploadConfigs = [
   { key: "market_indices", label: "Market Indices", apiEndpoint: "upload_market_index", buttonColor: "success" },
   { key: "DailyNoteUpload", label: "Daily Note Upload", apiEndpoint: "daily_note_deals_upload", buttonColor: "success" },
   { key: "risk_reward_upload", label: "Risk Reward Upload", apiEndpoint: "risk_reward_upload", buttonColor: "success" },
+  { key: "risk_reward_upload", label: "Risk Reward Upload", apiEndpoint: "risk_reward_upload", buttonColor: "success" },
 ];
 
 const MainUpload: React.FC = () => {
