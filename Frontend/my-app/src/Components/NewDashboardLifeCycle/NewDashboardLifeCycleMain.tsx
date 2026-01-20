40d3374a4d4d904282913c24a097fc9ad26ff69e
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-01-20 10:06:38 +0530

    added the new dashbaoard data life cycle

---
+++ Frontend/my-app/src/Components/NewDashboardLifeCycle/NewDashboardLifeCycleMain.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const NewDashboardLifeCycleMain = () => {
+  return (
+    <div>
+        <Typography>New Dashboard Life Cycle Main</Typography>
+      
+    </div>
+  )
+}
+
+export default NewDashboardLifeCycleMain
