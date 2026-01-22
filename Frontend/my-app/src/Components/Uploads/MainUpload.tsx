a8a78bcceb43800335a80ac4ee33f3988cdf4bf6
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-01-22 15:37:52 +0530

    remove the fo deals upload the data

--- Frontend/my-app/src/Components/Uploads/MainUpload.tsx
+++ Frontend/my-app/src/Components/Uploads/MainUpload.tsx
@@ -168,7 +168,6 @@ const MainUpload: React.FC = () => {
           <Tab label="Downloads" />
           <Tab label="Calendar" />
           <Tab label="LK File" />
-          <Tab label="FO Files" />
         </Tabs>
 
         <Box>
@@ -255,18 +254,6 @@ const MainUpload: React.FC = () => {
             <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
               <Card elevation={3} sx={{ borderRadius: 3, pt: 6, p: 2, background: "linear-gradient(to right, #ffecd2, #fcb69f)" }}>
                 <LkFileUpload />
-              </Card>
-            </motion.div>
-          )}
-
-          {selectedTab === 6 && (
-            <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
-              <Card elevation={3} sx={{ borderRadius: 3, p: 3, background: "linear-gradient(to right, #ffecd2, #fcb69f)" }}>
-                <Typography variant="h6" align="center" color="primary" sx={{ fontWeight: 600, mb: 2 }}>
-                  Upload FO Files
-                </Typography>
-                <Divider sx={{ my: 2 }} />
-                <FOS1FileUpload />
               </Card>
             </motion.div>
           )}
