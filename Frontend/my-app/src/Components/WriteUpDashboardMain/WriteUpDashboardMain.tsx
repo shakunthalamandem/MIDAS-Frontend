9f35684a4e4918eb98c002ca45554938b739292d
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-01-29 16:33:43 +0530

    added

--- Frontend/my-app/src/Components/WriteUpDashboardMain/WriteUpDashboardMain.tsx
+++ Frontend/my-app/src/Components/WriteUpDashboardMain/WriteUpDashboardMain.tsx
@@ -1,8 +1,10 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const WriteUpDashboardMain = () => {
   return (
     <div>
+        <Typography>Hey </Typography>
       
     </div>
   )
