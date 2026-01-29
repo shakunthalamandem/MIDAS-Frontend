7c4662a441c35dc299b014432cbd41ec63974607
charangoldenhills <charan.d@goldenhillsindia.com>
2026-01-29 12:50:20 +0530

    basic component setup for new dashboard

---
+++ Frontend/my-app/src/Components/NewDashboardLifeCycle/RecentDealRecomendation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecentDealRecomendation = () => {
+  return (
+    <div>
+      <h1>Recent Deal Recommendation Component</h1>
+    </div>
+  )
+}
+
+export default RecentDealRecomendation
---
+++ Frontend/my-app/src/Components/NewDashboardLifeCycle/UpcomingDealRecomendation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UpcomingDealRecomendation = () => {
+  return (
+    <div>
+        <h1>Upcoming Deal Recommendation Component</h1>
+      
+    </div>
+  )
+}
+
+export default UpcomingDealRecomendation
