5f71a31378d68f2a227d15526a84404a3325c2ba
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-01-30 10:25:03 +0530

    added the basic setup of the data

--- Frontend/my-app/src/Components/WriteUpDashboardMain/IPOWriteUpMetaData/IPOWriteUpMetaDataValuationAnalysis.tsx
+++ Frontend/my-app/src/Components/WriteUpDashboardMain/IPOWriteUpMetaData/IPOWriteUpMetaDataValuationAnalysis.tsx
@@ -1,5 +1,7 @@
+import { useMemo } from "react"
+import IPOValuationSection from "../../IPODashboardLLM/IPOValuationSection"
+import { SelectedData } from "../../IPODashboardLLM/IPODealsS1DealData"
 import { BasicDealDetails } from "../types/DealInformation"
-import IPOWriteUpMetaDataSectionCard from "./IPOWriteUpMetaDataSectionCard"
 
 interface IPOWriteUpMetaDataValuationAnalysisProps {
   basicDealDetails: BasicDealDetails
@@ -7,11 +9,19 @@ interface IPOWriteUpMetaDataValuationAnalysisProps {
 
 const IPOWriteUpMetaDataValuationAnalysis: React.FC<
   IPOWriteUpMetaDataValuationAnalysisProps
-> = ({ basicDealDetails }) => (
-  <IPOWriteUpMetaDataSectionCard
-    title="Valuation Analysis"
-    basicDealDetails={basicDealDetails}
-  />
-)
+> = ({ basicDealDetails }) => {
+  const selectedData: SelectedData = useMemo(
+    () => ({
+      ticker_name: basicDealDetails.ticker,
+      pricing_date: basicDealDetails.pricing_date
+    }),
+    [basicDealDetails.pricing_date, basicDealDetails.ticker]
+  )
+
+  return (
+
+      <IPOValuationSection selectedData={selectedData} />
+  )
+}
 
 export default IPOWriteUpMetaDataValuationAnalysis
