9485c48e16e800f89d7bf05e7f93ec109a34c6a9
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-02-12 11:05:24 +0530

    ADDED THE NEW KEY METRICS

--- Frontend/my-app/src/Components/WriteUpDashboardMain/FebIPOWriteUpDashboardMain.tsx
+++ Frontend/my-app/src/Components/WriteUpDashboardMain/FebIPOWriteUpDashboardMain.tsx
@@ -297,7 +297,7 @@ const FebIPOWriteUpDashboardMain: React.FC<FebIPOWriteUpDashboardMainProps> = ({
 
             <Card id="key-metrics" sx={sectionCardSx}>
               <CardContent sx={{ p: { xs: 2, md: 2.5 } }}>
-                <IPOWriteUpMetaDataKeyMetrics
+                <IPOWriteUpMetaDataKeyMetricsNew
                   basicDealDetails={basicDealDetails}
                 />
               </CardContent>
