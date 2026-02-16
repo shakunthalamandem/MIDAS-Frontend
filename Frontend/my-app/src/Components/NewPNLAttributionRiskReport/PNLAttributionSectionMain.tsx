8cc65d95a3119673c10fa2cc8a1db78ef777d61e
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-02-16 18:55:21 +0530

    removed the unsed code format

--- Frontend/my-app/src/Components/NewPNLAttributionRiskReport/PNLAttributionSectionMain.tsx
+++ Frontend/my-app/src/Components/NewPNLAttributionRiskReport/PNLAttributionSectionMain.tsx
@@ -21,34 +21,9 @@ const PNLAttributionSectionMain = () => {
 
   return (
     <Container maxWidth="xl" sx={{ padding: 0, marginBottom: 4 }}>
-      <Typography
-        variant="h3"
-        sx={{
-          fontWeight: 500,
-          color: "#FFFFFF",
-          fontSize: { xs: "1rem", sm: "1.2rem" },
-          backgroundColor: "#002060",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "4vh",
-          padding: "8px 16px",
-          borderRadius: "8px",
-          textAlign: "center",
-          marginBottom: "20px",
-          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-          animation: "fadeIn 1.5s ease-in-out",
-          "@keyframes fadeIn": {
-            "0%": { opacity: 0 },
-            "100%": { opacity: 1 },
-          },
-        }}
-      >
-        PNL Attribution & Risk Report
-      </Typography>
 
-      <Card sx={{ boxShadow: 3, borderRadius: 2, mb: 3 }}>
-        <CardContent sx={{ p: 0 }}>
+
+
           <Tabs
             value={tabValue}
             onChange={handleTabChange}
@@ -87,8 +62,7 @@ const PNLAttributionSectionMain = () => {
             <Tab label="PNL Attribution" />
             <Tab label="Risk Report" />
           </Tabs>
-        </CardContent>
-      </Card>
+
 
       <Card
         sx={{
