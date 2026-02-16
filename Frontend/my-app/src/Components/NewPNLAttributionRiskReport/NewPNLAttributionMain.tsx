ab9d98f843823308c2a2fd507c4bf9637dbd1963
anumula-ranjith <ranjith.a@goldenhillsindia.com>
2026-02-16 16:42:18 +0530

    added the  pnl risk

--- Frontend/my-app/src/Components/NewPNLAttributionRiskReport/NewPNLAttributionMain.tsx
+++ Frontend/my-app/src/Components/NewPNLAttributionRiskReport/NewPNLAttributionMain.tsx
@@ -1,14 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import RiskDashboard from "./PNLAttributionData/RiskDashboard";
 
 const NewPNLAttributionMain = () => {
   return (
     <Box sx={{ p: 2 }}>
-      <Typography
-        variant="h6"
-        sx={{ fontWeight: 600, color: "#002060", mb: 2 }}
-      >
-        PNL Attribution
-      </Typography>
+      <RiskDashboard />
     </Box>
   );
 };
