8e3687750d46d13279c496844d9cbd63d06ae0e6
shakunthalamandem <shakunthala.m@goldenhillsindia.com>
2026-03-31 15:05:52 +0530

    added the correct naming  of gator for jay ritter

--- Frontend/my-app/src/Routes/AppRouters.tsx
+++ Frontend/my-app/src/Routes/AppRouters.tsx
@@ -335,7 +335,7 @@ const AppRouters: React.FC = () => {
         <Route path='/ai_portfolio_review' element={<AuthGuard><AIPortfolioReview mode="portfolioReview" reviewTab="portfolio" /></AuthGuard>} />
         <Route path='/ai_risk_review' element={<AuthGuard><AIPortfolioReview mode="portfolioReview" reviewTab="risk" /></AuthGuard>} />
         <Route path='/last_30_days_ai_ranking' element={<AuthGuard><AIPortfolioReview mode="stockRanking" /></AuthGuard>} />
-        <Route path='/jay_ritter_ipo_analysis' element={<AuthGuard><JayRitterIPOAnalysis /></AuthGuard>} />
+        <Route path='/gator_ipo_analysis' element={<AuthGuard><JayRitterIPOAnalysis /></AuthGuard>} />
 
         {/* <Route path="/agents" element={<AuthGuard><Agents /></AuthGuard>} /> */}
         <Route path="/agents/dashboard" element={<AuthGuard><Agents /></AuthGuard>} />
